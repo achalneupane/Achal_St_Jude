7eb741f80a2ed3ecb909ee7b938297dadc00c273
Neupane <achalneupane@gmail.com>
2022-05-17 09:58:04 -0500

    .

--- rcodes/variants_counts.R
+++ rcodes/variants_counts.R
@@ -15,7 +15,7 @@ var.classes
 setwd("Z:/ResearchHome/Groups/sapkogrp/projects/SJLIFE_WGS/common/sjlife/MERGED_SJLIFE_1_2/annotation/SNPEFF_ANNOTATION/")
 ## read annotated SJLIFE annotated VCF 
 library(data.table)
-VCF <- fread("MERGED.SJLIFE.1.2.GATKv3.4.VQSR.chr22.PASS.decomposed.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155-FIELDS-simple.txt")
+VCF <- fread("MERGED.SJLIFE.1.2.GATKv3.4.VQSR.chr17.PASS.decomposed.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155-FIELDS-simple.txt")
 table(VCF$CLNSIG)
 
 WANTED.types <- c("$Pathogenic/Likely_pathogenic$|^Likely_pathogenic$|^Pathogenic/Likely_pathogenic$|^Pathogenic$")
@@ -24,4 +24,3 @@ sum(grepl(WANTED.types, VCF$CLNSIG, ignore.case = T))
 
 WANTED.vars.clinvar <- VCF[grepl(WANTED.types, VCF$CLNSIG, ignore.case = T),]
 
-                      
