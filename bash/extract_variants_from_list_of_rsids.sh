104381b2bf4115495ddf5eeeb9fc3d5c3f8aeaec
Neupane <achalneupane@gmail.com>
2022-05-19 12:39:34 -0500

    .

--- bash/extract_variants_from_list_of_rsids.sh
+++ bash/extract_variants_from_list_of_rsids.sh
@@ -45,24 +45,92 @@ df$SNP_extract <- paste(df$X.chrom,df$chromEnd, sep = ":")
 
 ## Extract these variants with PLINK (there could be allele flips, so extracting by chr and pos)
 write.table(unique(df$SNP_extract), "/ResearchHome/Groups/sapkogrp/projects/SJLIFE_WGS/common/sjlife/MERGED_SJLIFE_1_2/MERGED_SJLIFE_PLINK_PER_CHR/tasks/task_5_17_2022/SNPs_to_extract_05-18-2022-task.txt", row.names = F, col.names = F, quote = FALSE)
+write.table(df$SNP, "/ResearchHome/Groups/sapkogrp/projects/SJLIFE_WGS/common/sjlife/MERGED_SJLIFE_1_2/MERGED_SJLIFE_PLINK_PER_CHR/tasks/task_5_17_2022/SNPs_to_extract_complete_05-18-2022-task.txt", row.names = F, col.names = F, quote = FALSE)
 ```
 
 # Check which ones are multi allelic in our data
 for line in $(cat SNPs_to_extract_05-18-2022-task.txt); do
-echo "---- DOING ${line} ---"
-grep -w ${line} MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr*.PASS.decomposed_geno.0.1_hwe.1e-10.bim
-done
+echo -e "$(grep -w ${line} MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr*.PASS.decomposed_geno.0.1_hwe.1e-10.bim)\tin_position_$line"
+done >> test.txt
+
+echo "$(grep -R $id) appearances in database"
 
 ## This is multi allelic:
 # ---- DOING chr2:226203364 ---
 # MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr2.PASS.decomposed_geno.0.1_hwe.1e-10.bim:2     chr2:226203364:A:C      0       226203364       C       A
 # MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr2.PASS.decomposed_geno.0.1_hwe.1e-10.bim:2     chr2:226203364:A:G      0       226203364       G       A
 
+[aneupane@splprhpc05 task_5_17_2022]$ grep 226203364 yadav_rs_id_to_var_position.txt
+chr2    226203364       rs2943634       A       C,G,
+
+## Checking freq for these variants, so I will run --freq on chr2
+plink --bfile MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr2.PASS.decomposed_geno.0.1_hwe.1e-10 --freq  --out chr2_freq
+# [aneupane@noderome156 task_5_17_2022]$ egrep 'chr2:226203364:A:C|chr2:226203364:A:G' chr2_freq.frq
+#    2 chr2:226203364:A:C    A    C       0.3746     8956
+#    2 chr2:226203364:A:G    G    A     0.005695     8956
+
+## Now check in 1000 genome population.
+plink --bfile /research_jude/rgs01_jude/groups/sapkogrp/projects/SJLIFE_WGS/common/1kGP/plink/ALL.chr2_GRCh38.genotypes.20170504_biallelic_uniq_chrpos --freq --out 1000genomes_chr2_freq
+plink --bfile /research_jude/rgs01_jude/groups/sapkogrp/projects/SJLIFE_WGS/common/sjlife/MERGED_SJLIFE_1_2/MERGED_SJLIFE_PLINK_PER_CHR/1000genomes_merged --freq --out 1000genomes_merged_freq
+
+# I was able to find this variant in one of the 1000genomes files by rs ID but not by position.
+grep rs2943634 1000genomes_merged_freq.frq 
+# [aneupane@splprhpc05 task_5_17_2022]$ grep rs2943634 1000genomes_merged_freq.frq
+#    2  rs2943634    A    C       0.3209     4986
+grep 2:226203364 1000genomes_chr2_freq.frq
+# https://gnomad.broadinstitute.org/variant/rs2943634?dataset=gnomad_r3
+
+## I then checked if all these variants are in same allele order in our SJLIFE dataset
+grep MERGED test.txt| cut -d$'\t' -f2> found_variants.txt
+wc -l found_variants.txt
+# 44 found_variants.txt
+for VAR in $(cat found_variants.txt); do
+echo "Doing Var ${VAR}"
+grep -w ${VAR} MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr*.PASS.decomposed_geno.0.1_hwe.1e-10.bim
+done
+
+# Since 1KG has higher freq for chr2:226203364:A:C, I am removing chr2:226203364:A:G from my list.
 
 
 # Extract variants from plink
+grep -v chr2:226203364:A:G found_variants.txt > final_var.txt
 module load plink/1.90b
 for i in {1..22}; do
 BFILE="MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr${i}.PASS.decomposed_geno.0.1_hwe.1e-10"	
-plink --bfile ${BFILE} --extract SNPs_to_extract_05-18-2022-task.txt --keep-allele-order --make-bed --out CHR_${i}
+plink --bfile ${BFILE} --extract final_var.txt --keep-allele-order --make-bed --out CHR_${i}
 done
+
+# See which logs generated errors for no variants remaining
+grep -i Error CHR_*.log; exclude 12, 13, 18 from merge list
+
+for i in {2..11} {14..17} {19..22}; do
+echo "CHR_$i"	>> merge_list.list
+done
+
+
+plink --bfile  CHR_1 --merge-list merge_list.list --make-bed --out CHR_ALL
+
+# plink --bfile <Bed file prefix> --extract <File contain SNP ID> --recode --out <name of output>
+plink --bfile CHR_ALL --recode --out GENOTYPE
+
+# Now read .ped file and transpose .map to get the genotype using R
+PED <- read.table("GENOTYPE.ped")
+PED <- PED[-c(2:6)]
+MAP <- read.table("GENOTYPE.map")
+# Add rsIDs to MAP
+MAP$rsID <- df$name[match(MAP$V2, df$SNP)]
+
+colnames(PED) <- c("IID", rbind(paste0("ALT_", MAP$rsID), paste0("REF_", MAP$rsID)))
+## Re-arrange columns
+PED <- PED[c("IID", rbind(paste0("REF_", MAP$rsID), paste0("ALT_", MAP$rsID)))]
+
+## Paste two columns together
+i1 <- seq(1, length(PED)-1, 2)
+i2 <- seq(2, length(PED)-1, 2)
+
+PED <- as.data.table(PED)
+
+PED[, Map(paste,
+         .SD[, i1, with = FALSE], .SD[, i2, with = FALSE], 
+         MoreArgs = list(sep="-")), 
+   by = "IID"]
