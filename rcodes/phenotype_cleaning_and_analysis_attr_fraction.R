fa2213ec51d39b32fc99402bce782653e53926ed
Neupane <achalneupane@gmail.com>
2022-07-13 16:45:24 -0500

    .

--- rcodes/phenotype_cleaning_and_analysis_attr_fraction.R
+++ rcodes/phenotype_cleaning_and_analysis_attr_fraction.R
@@ -175,17 +175,46 @@ clinical.dat[grepl("anyrt_|AnyRT", colnames(clinical.dat))][clinical.dat[grepl("
 ########################
 ## Merge Genetic data ##
 ########################
+
 QIN_vars <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/genetic_data/Na_Qin_vars/Qin_et_al_all_vars_final_recodeA.raw", header = T)
 QIN_vars <- QIN_vars[-grep("FID|PAT|MAT|SEX|PHENOTYPE", colnames(QIN_vars))]
 QIN_vars$Qin_Non.Ref.Counts <- rowSums(QIN_vars[-1]) 
 clinical.dat$Qin_Non.Ref.Counts <- QIN_vars$Qin_Non.Ref.Counts [match(clinical.dat$sjlid, QIN_vars$IID)]
 clinical.dat$Qin_carriers <- ifelse(clinical.dat$Qin_Non.Ref.Counts > 0, "Y", "N")
 
+
 Zhaoming_vars <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/genetic_data/Zhaoming_Wang_vars/Zhaoming_et_al_all_vars_final_recodeA.raw", header = T)
 Zhaoming_vars <- Zhaoming_vars[-grep("FID|PAT|MAT|SEX|PHENOTYPE", colnames(Zhaoming_vars))]
+# edit column names
+colnames(Zhaoming_vars) <- str_split(gsub("\\.", ":", colnames(Zhaoming_vars)), "_", simplify=T)[,1]
+
+## 
+# Sys.setlocale("LC_ALL", "C")
+zhaoming_tables <- read.delim("/ResearchHome/ClusterHome/aneupane/St_Jude/Yadav_Sapkota/additional_papers/Zhaoming_Wang_Genetic_risk_for_subsequent_neoplasms_JCO.2018.77.8589/P-PL-sjlife-genetics-sn_SNV_INDELS_07_13_2022.txt", sep = "\t")
+dim(zhaoming_tables)
+sum(!duplicated(zhaoming_tables$FULLKEY))
+search_list <- read.delim("/ResearchHome/ClusterHome/aneupane/St_Jude/Yadav_Sapkota/additional_papers/Zhaoming_Wang_Genetic_risk_for_subsequent_neoplasms_JCO.2018.77.8589/search_list.txt", sep = "\t")
+sum(unique(zhaoming_tables$FULLKEY) %in% search_list$KEY.varID)
+# 299
+tableA4 <- zhaoming_tables[grepl ("A4", zhaoming_tables$Table),]
+dim(tableA4)
+# 166
+tableA10 <- zhaoming_tables[grepl ("A10", zhaoming_tables$Table),]
+sum(unique(search_list$KEY.varID) %in% unique(tableA4$FULLKEY))
+# 148
+search_list <- search_list[search_list$KEY.varID %in% tableA4$FULLKEY,]
+
+# sum(tableA4$Gene %in% tableA10$Gene)
+# sum(tableA4$FULLKEY %in% tableA10$FULLKEY)
+
+
+# Keeping variants in Table A4 only
+Zhaoming_vars <- Zhaoming_vars[c(1,which(colnames(Zhaoming_vars) %in% search_list$ID))]
 Zhaoming_vars$Zhaoming_Non.Ref.Counts <- rowSums(Zhaoming_vars[-1]) 
+
 clinical.dat$Zhaoming_Non.Ref.Counts <- Zhaoming_vars$Zhaoming_Non.Ref.Counts [match(clinical.dat$sjlid, Zhaoming_vars$IID)]
 clinical.dat$Zhaoming_carriers <- ifelse(clinical.dat$Zhaoming_Non.Ref.Counts > 0, "Y", "N")
+
 
 # Add ethnicity from PCA
 EUR <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/sjlife/MERGED_SJLIFE_1_2/MERGED_SJLIFE_PLINK_PER_CHR/PCA/SJLIFE_EUR_Per_PCA.txt", stringsAsFactors = F, header = F)
@@ -197,6 +226,12 @@ clinical.dat$PCA.ethnicity <- ifelse(clinical.dat$sjlid %in% EUR$V1, "EUR", "")
 clinical.dat$PCA.ethnicity[!grepl("EUR", clinical.dat$PCA.ethnicity)]    <- ifelse(clinical.dat$sjlid[!grepl("EUR", clinical.dat$PCA.ethnicity)] %in% AFR$V1, "AFR", "")
 clinical.dat$PCA.ethnicity[!grepl("EUR|AFR", clinical.dat$PCA.ethnicity)]    <- ifelse(clinical.dat$sjlid[!grepl("EUR|AFR", clinical.dat$PCA.ethnicity)] %in% EAS$V1, "EAS", "")
 
+
+dim(QIN_vars)
+dim(clinical.dat)
+
+sum(QIN_vars$IID %in% clinical.dat$sjlid)
+# 4401
 # test <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/genetic_data/test.txt", header =T, check.names = F,   colClasses = c("character"))
 # test <- as.data.frame(t(test))
 # test$row.NAMES <- as.character(rownames(test))
@@ -375,19 +410,44 @@ PHENO.ANY_SN$Epidophyllotoxin [PHENO.ANY_SN$epitxn_dose_any > 6704] <- "3rd"
 PHENO.ANY_SN$Epidophyllotoxin <- factor(PHENO.ANY_SN$Epidophyllotoxin, levels = c("None", "1st", "2nd", "3rd"))
 
 
-
 PHENO.ANY_SN.EUR <- PHENO.ANY_SN[PHENO.ANY_SN$PCA.ethnicity == 'EUR', -grep("sjlid|PCA.ethnicity|AGE.ANY_SN", colnames(PHENO.ANY_SN))]
 
 
 
-mod1 <- glm(ANY_SN ~ Qin_carriers + gender + agedx + AnyRT + maxseg1dose, family = binomial(link = "logit"), data = PHENO.ANY_SN.EUR)
-
-mod1 <- glm(ANY_SN ~ . , family = binomial(link = "logit"), data = PHENO.ANY_SN.EUR)
+mod1 <- glm(ANY_SN ~ Zhaoming_carriers + AGE_AT_DIAGNOSIS + gender + brainrt_yn + chestrt_yn + abdomenrt_yn + Epidophyllotoxin, family = binomial(link = "logit"), data = PHENO.ANY_SN)
 summary(mod1)
 
-mod1 <- glm(ANY_SN ~ Zhaoming_carriers + AGE_AT_DIAGNOSIS + gender + brainrt_yn + neckrt_yn + chestrt_yn + abdomenrt_yn + pelvisrt_yn + Alkylating_agent + Epidophyllotoxin, family = binomial(link = "logit"), data = PHENO.ANY_SN.EUR)
-summary(mod1)
 
+mod1.EUR <- glm(ANY_SN ~ Zhaoming_carriers + AGE_AT_DIAGNOSIS + gender + brainrt_yn + chestrt_yn + abdomenrt_yn + Epidophyllotoxin, family = binomial(link = "logit"), data = PHENO.ANY_SN.EUR)
+summary(mod1.EUR)
+
+
+## Prevalence
+prevalence.counts <- sum(Zhaoming_vars$Zhaoming_Non.Ref.Counts > 0)
+prop.test(prevalence.counts, 4507)
+
+#########################################
+## Sjlife sample list (used by Zhaoming); checking only in these samples
+sjlife1.samples <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/sjlife_1/sample_list.txt")
+PHENO.ANY_SN.sjlife1 <- PHENO.ANY_SN[PHENO.ANY_SN$sjlid %in% sjlife1.samples$V1,]
+PHENO.ANY_SN.sjlife1.EUR <- PHENO.ANY_SN.sjlife1[PHENO.ANY_SN.sjlife1$PCA.ethnicity == 'EUR', -grep("sjlid|PCA.ethnicity|AGE.ANY_SN", colnames(PHENO.ANY_SN.sjlife1))]
+
+mod.sjlife1 <- glm(ANY_SN ~ Zhaoming_carriers + AGE_AT_DIAGNOSIS + gender + brainrt_yn + chestrt_yn + abdomenrt_yn + Epidophyllotoxin, family = binomial(link = "logit"), data = PHENO.ANY_SN.sjlife1)
+summary(mod.sjlife1)
+
+mod.sjlife1.EUR <- glm(ANY_SN ~ Zhaoming_carriers + AGE_AT_DIAGNOSIS + gender + brainrt_yn + chestrt_yn + abdomenrt_yn + Epidophyllotoxin, family = binomial(link = "logit"), data = PHENO.ANY_SN.sjlife1.EUR)
+summary(mod.sjlife1.EUR)
+
+# also checking prevalence
+Zhaoming_vars.sjlife1 <- Zhaoming_vars[Zhaoming_vars$IID %in% sjlife1.samples$V1,]
+table(ifelse(Zhaoming_vars.sjlife1$Zhaoming_Non.Ref.Counts > 0, "Y", "N"))
+# N    Y 
+# 2664  322 
+
+prop.test(375,4132,correct=FALSE)
+
+
+########################################
 
 
 
