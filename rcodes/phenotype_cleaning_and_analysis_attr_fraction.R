e8904d20fbf6799009b5ebfdcdf57210f66eb81d
Neupane <achalneupane@gmail.com>
2022-07-25 17:28:27 -0500

    .

--- rcodes/phenotype_cleaning_and_analysis_attr_fraction.R
+++ rcodes/phenotype_cleaning_and_analysis_attr_fraction.R
@@ -943,25 +943,25 @@ qi.df.final <- rbind.data.frame(qi.df.not.SN, qi.df.SN.filtered)
 dim(qi.df.final)
 qi.df.final <- qi.df.final[mixedorder(qi.df.final$sjlid),]
 
-paste0(as.numeric(table(qi.df.final$anyrt_5)[2]), ", ", as.numeric(table(pheno.crosscheck$anyrt_5)[2])) # anyrt_5
+paste0(table(qi.df.final$anyrt_5)[2], ", ", table(pheno.crosscheck$anyrt_5)[2]) # anyrt_5
 # "2169, 2174"
-paste0(as.numeric(table(qi.df.final$brainrt_yn)[2]), ", ", as.numeric(table(pheno.crosscheck$brainrt_yn)[2])) # Brain
+paste0(table(qi.df.final$brainrt_yn)[2], ", ", table(pheno.crosscheck$brainrt_yn)[2]) # Brain
 # "858, 1095"
-paste0(as.numeric(table(qi.df.final$neckrt_yn)[2]), ", ", as.numeric(table(pheno.crosscheck$neckrt_yn)[2])) # Neck
+paste0(table(qi.df.final$neckrt_yn)[2], ", ", table(pheno.crosscheck$neckrt_yn)[2]) # Neck
 # "601, 797"
-paste0(as.numeric(table(qi.df.final$chest_yn)[2]), ", ", as.numeric(table(pheno.crosscheck$chestrt_yn)[2])) # Chest
+paste0(table(qi.df.final$chest_yn)[2], ", ", table(pheno.crosscheck$chestrt_yn)[2]) # Chest
 # 628, 861
-paste0(as.numeric(table(qi.df.final$abdomenrt_yn)[2]), ", ", as.numeric(table(pheno.crosscheck$abdomenrt_yn)[2])) # Abdomen
+paste0(table(qi.df.final$abdomenrt_yn)[2], ", ", table(pheno.crosscheck$abdomenrt_yn)[2]) # Abdomen
 # "567, 790"
-paste0(as.numeric(table(qi.df.final$pelvis_yn)[2]), ", ", as.numeric(table(pheno.crosscheck$pelvisrt_yn)[2])) # Pelvis
+paste0(table(qi.df.final$pelvis_yn)[2], ", ", table(pheno.crosscheck$pelvisrt_yn)[2]) # Pelvis
 # "506, 688"
-paste0(as.numeric(table(qi.df.final$aaclassic_5 ==1)[2]), ", ", as.numeric(table(pheno.crosscheck$aa_class_dose_5_yn)[2])) # Alkylating_5
+paste0(table(qi.df.final$aaclassic_5 ==1)[2], ", ", table(pheno.crosscheck$aa_class_dose_5_yn)[2]) # Alkylating_5
 # "2480, 2473"
 
 
-paste0(as.numeric(table(qi.df.final$anthracyclines_5 > 0)[2]), ", ", as.numeric(table(pheno.crosscheck$anthra_jco_dose_5_yn)[2])) # Anthracyclines_5
+paste0(table(qi.df.final$anthracyclines_5 > 0)[2], ", ", table(pheno.crosscheck$anthra_jco_dose_5_yn)[2]) # Anthracyclines_5
 # "2455, 2451"
-paste0(as.numeric(table(qi.df.final$epipodophyllotoxins_5 > 0)[2]), ", ", as.numeric(table(pheno.crosscheck$epitxn_dose_5_yn)[2])) # Epipodophyllotoxins_5
+paste0(table(qi.df.final$epipodophyllotoxins_5 > 0)[2], ", ", table(pheno.crosscheck$epitxn_dose_5_yn)[2]) # Epipodophyllotoxins_5
 # "1496, 1488"
 
 # Age at diagnosis
