12a34d3c4a75d757b8c60ed442265b4d126fb897
Neupane <achalneupane@gmail.com>
2022-08-01 14:24:36 -0500

    .

--- rcodes/attributable_fraction_R_codes/4.a_subsequent_neoplasm_Any_SN.r
+++ rcodes/attributable_fraction_R_codes/4.a_subsequent_neoplasm_Any_SN.r
@@ -87,7 +87,6 @@ PHENO.ANY_SN.EUR <- PHENO.ANY_SN[PHENO.ANY_SN$PCA.ethnicity == 'EUR', -grep("sjl
 PHENO.ANY_SN.AFR <- PHENO.ANY_SN[PHENO.ANY_SN$PCA.ethnicity == 'AFR', -grep("sjlid|PCA.ethnicity|AGE.ANY_SN", colnames(PHENO.ANY_SN))]
 
 
-
 #################
 ## MODEL TESTS ##
 #################
@@ -104,5 +103,71 @@ mod1.EUR <- glm(ANY_SN ~ AGE_AT_LAST_CONTACT.cs1 + AGE_AT_LAST_CONTACT.cs2 + AGE
 summary(mod1.EUR)
 
 
+######################################
+## Attributable fraction of Any SNs ##
+######################################
+
+## Predicted prevalence of ANY_SN
+dat_all = PHENO.ANY_SN
+# Fit the model - this needs to be the final model with all the variables of interest
+fit_all = glm(formula = ANY_SN ~ Zhaoming_carriers + Qin_carriers.HR.pathways + Qin_carriers.FA.pathways + Qin_carriers.MMR.pathways + 
+                Qin_carriers.BER.pathways + Qin_carriers.NER.pathways + Qin_carriers.NHEJ.pathways + AGE_AT_LAST_CONTACT.cs1 +
+                AGE_AT_LAST_CONTACT.cs2 + AGE_AT_LAST_CONTACT.cs3 + AGE_AT_LAST_CONTACT.cs4 +
+                AGE_AT_DIAGNOSIS + gender + maxsegrtdose.category + maxabdrtdose.category +
+                maxchestrtdose.category + epitxn_dose_5.category + Pleiotropy_Bi_directional_Increasing_Sig_PRS + 
+                Pleiotropy_Bi_directional_Decreasing_Sig_PRS, family = binomial,
+              data = dat_all)
+
+# Get predicted values
+dat_all$pred_all = predict(fit_all, newdat = dat_all, type = "response")
+
+## Move relevant treatment exposures for everyone to no exposure
+dat_tx = dat_all
+dat_tx$maxsegrtdose.category = dat_tx$maxabdrtdose.category = dat_tx$maxchestrtdose.category = dat_tx$epitxn_dose_5.category = "None"
+dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
+
+## Attributable fraction calculation
+# First get the "predicted" number of SNs
+# Based on the model including all variables
+N_all = sum(dat_all$pred_all, na.rm = TRUE)
+N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
+af_by_tx = (N_all - N_no_tx) / N_all
+print(af_by_tx)
+# paste0(round(af_by_tx *100, 2), "%")
+
+## P/LP
+dat_plp = dat_all
+dat_plp$Zhaoming_carriers = "N"
+
+dat_all$pred_no_plp = predict(fit_all, newdata = dat_plp, type = "response")
+N_no_plp = sum(dat_all$pred_no_plp, na.rm = TRUE)
+af_by_plp = (N_all - N_no_plp) / N_all
+print(af_by_plp)
+
+# # H.C.Clin.LoF.Non.Ref.Counts
+# dat_plp = dat_all
+# dat_plp$H.C.Clin.LoF.Non.Ref.Counts = 0
+# 
+# dat_all$pred_no_H.C.Clin.LoF.Non.Ref.Counts = predict(fit_all, newdata = dat_plp, type = "response")
+# N_no_pred_no_H.C.Clin.LoF.Non.Ref.Counts = sum(dat_all$pred_no_H.C.Clin.LoF.Non.Ref.Counts, na.rm = TRUE)
+# af_by_H.C.Clin.LoF.Non.Ref.Counts = (N_all - N_no_pred_no_H.C.Clin.LoF.Non.Ref.Counts) / N_all
+# print(af_by_H.C.Clin.LoF.Non.Ref.Counts)
 
 
+# Pleiotropy_Bi_directional_Increasing_Sig_PRS
+dat_plp = dat_all
+dat_plp$Pleiotropy_Bi_directional_Increasing_Sig_PRS = 0
+
+dat_all$pred_no_Pleiotropy_Bi_directional_Increasing_Sig_PRS = predict(fit_all, newdata = dat_plp, type = "response")
+N_no_pred_no_Pleiotropy_Bi_directional_Increasing_Sig_PRS = sum(dat_all$pred_no_Pleiotropy_Bi_directional_Increasing_Sig_PRS, na.rm = TRUE)
+af_by_Pleiotropy_Bi_directional_Increasing_Sig_PRS = (N_all - N_no_pred_no_Pleiotropy_Bi_directional_Increasing_Sig_PRS) / N_all
+print(af_by_Pleiotropy_Bi_directional_Increasing_Sig_PRS)
+
+# Pleiotropy_Bi_directional_Decreasing_Sig_PRS
+dat_plp = dat_all
+dat_plp$Pleiotropy_Bi_directional_Decreasing_Sig_PRS = 0
+
+dat_all$pred_no_Pleiotropy_Bi_directional_Decreasing_Sig_PRS = predict(fit_all, newdata = dat_plp, type = "response")
+N_no_pred_no_Pleiotropy_Bi_directional_Decreasing_Sig_PRS = sum(dat_all$pred_no_Pleiotropy_Bi_directional_Decreasing_Sig_PRS, na.rm = TRUE)
+af_by_Pleiotropy_Bi_directional_Decreasing_Sig_PRS = (N_all - N_no_pred_no_Pleiotropy_Bi_directional_Decreasing_Sig_PRS) / N_all
+print(af_by_Pleiotropy_Bi_directional_Decreasing_Sig_PRS)
