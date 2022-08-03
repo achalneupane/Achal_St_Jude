e81a98997fa8831b04cb5c59c70fbedd7b873a2d
Neupane <achalneupane@gmail.com>
2022-08-03 15:30:04 -0500

    .

--- bash/TTN_BAG3_analysis.sh
+++ bash/TTN_BAG3_analysis.sh
@@ -3,6 +3,8 @@
 cd /research_jude/rgs01_jude/groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3
 # To calculate maf, use sjlife.fam on /research_jude/rgs01_jude/groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3. Use pheno/sjlife_ttn_bag3.pheno
 awk '{print $1"\t"$2}' pheno/sjlife_ttn_bag3.pheno > samples_for_maf.txt
+awk '{print $1"\t"$2}' pheno/ccss_org_eur_cardiotoxic_exposed.pheno > samples_for_maf_ccss_org.txt
+awk '{print $1"\t"$2}' pheno/ccss_exp_eur_cardiotoxic_exposed.pheno > samples_for_maf_ccss_exp.txt
 
 # USE R code TITN_BAG3.r
 # Chunk 1.
@@ -12,27 +14,23 @@ awk '{print $1"\t"$2}' pheno/sjlife_ttn_bag3.pheno > samples_for_maf.txt
 cd /research_jude/rgs01_jude/groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64
 ln -s ../../samples_for_maf.txt
 
-awk '{$2 = "chr"$1":"$4; print}' sjlife.bim > sjlife_edited.bim
 
 module load plink/1.90b
-plink --bfile sjlife --bim sjlife.bim --extract samplesnp.list --keep samples_for_maf.txt --make-bed --out samplesnp.dat
+plink --bfile sjlife --extract samplesnp.list --keep samples_for_maf.txt --keep-allele-order --make-bed --out samplesnp.dat
 
-plink --bfile samplesnp.dat --keep samples_for_maf.txt --freq --out sjlife.freq.out
-
-# NOt sure how to handle grep 10:119485655:A:ATATTTTATTTTATTT sjlife.freq.out.frq
-
+plink --bfile samplesnp.dat --keep samples_for_maf.txt --freq --keep-allele-order --out sjlife.freq.out
 
 # First removing trailing spaces in the file, then removing chr
 awk '{ for(i=1;i<=NF;i++){if(i==NF){printf("%s\n",$NF);}else {printf("%s\t",$i)}}}' sjlife.freq.out.frq | awk -F " " '{gsub(/chr/, "", $2); print}' OFS="\t" > sjlife.freq.out.frq_edited1
 
 
-plink --r2 --bfile sjlife --extract samplesnp.list --keep samples_for_maf.txt --matrix --out samplesnp
+plink --r2 --bfile samplesnp.dat --matrix --out samplesnp
 ## samplesnp.list
 # chr2:178802888:A:G
 # chr2:178803079:C:T
 # chr2:178804123:G:A
 # ...
-# NOTE: The order changes to the chr and position order in the SNP list. So, the summary statistics needs to be updated to maintain the order as in plink subset. 
+# NOTE: The order changes according to the chr and position order in the SNP list. So, the summary statistics needs to be updated to maintain the order as in plink subset. 
 
 # test1.list
 # chr2:178313079:G:A
@@ -64,13 +62,14 @@ plink --r2 --bfile sjlife --extract samplesnp.list --keep samples_for_maf.txt --
 z;ld;snp;config;cred;log;n_samples
 samplesnp.z;samplesnp.ld;samplesnp.snp;samplesnp.config;samplesnp.cred;samplesnp_finemap.log;1645
 
-./finemap_v1.4.1_x86_64 --sss --in-files samplesnp --dataset 1
-
 
 
 # I was getting errors with the file format, so I had to run dos2unix
 # chmod 777 *
 # dos2unix samplesnp.z
+./finemap_v1.4.1_x86_64 --sss --in-files samplesnp --dataset 1
+
+
 
 
 # Additionally, .z file shoul have maf in specific form. I was getting this error: "Error : Expected a floating point value greater than 0.0 and smaller or equal than 0.5 in line 3 column 6 of file 'samplesnp.z'!"
--- rcodes/TITN_BAG3_v2.R
+++ rcodes/TITN_BAG3_v2.R
@@ -16,47 +16,77 @@ sjlife.bim$KEY <- paste(sjlife.bim$V1, sjlife.bim$V4, sjlife.bim$V6, sjlife.bim$
 sum(gwas.dat$SNP %in% sjlife.bim$KEY)
 # 2601
 
+sjlife.bim$V2 <- gsub("chr", "", sjlife.bim$V2)
+
 sjlife.bim <- sjlife.bim[sjlife.bim$KEY %in% gwas.dat$SNP,]
-sjlife.bim$KEY[duplicated(sjlife.bim$KEY)]
+sjlife.bim$V2[duplicated(sjlife.bim$KEY)]
+
+## checked dbSNP and these seem to be the real ones
+# 10:119588327:TTTTC:T
+# 10:119427920:ATCACTGCCATCATCAGCC:A # matches A1 and A2 as in summary stat
+# 10:119485655:A:ATATTTTATTTTATTT
+# 10:119528844:T:TAATA; maf 0.07029 # matches A1 and A2 as in summary stat
+# 10:119614280:A:AAAAT; 
+# 10:119614280:A:AAAAT;10:119614280:A:AAAATAAATAAAT     AAAAT     A       0.1388  3270
+## There are variants 
+
+
+
+# Based on the conversation with Yadav on 08/03/2022, I am deleting 10:119614280
+# and 10:119485655 from the list. I am also going to swap alleles for
+# 10:119427920 so that minor alleles correspond to the effect estimate.
+
+# In conclusion,
+# 10:119427920:ATCACTGCCATCATCAGCC:A # Keep and swap alleles
+# 10:119427920:A:ATCACTGCCATCATCAGCC # remove
+# 10:119528844:T:TAATA # keep
+# 10:119528844:TAATA:T # remove
+# 10:119588327:TTTTC:T # keep
+# 10:119588327:T:TTTTC # remove
+# 10:119614280 # remove
+# 10:119485655 # remove
+
+
+
+sjlife.bim$V2[grepl("10:119528844|10:119427920|10:119588327", sjlife.bim$V2)]
+# [1] "10:119427920:A:ATCACTGCCATCATCAGCC" "10:119427920:ATCACTGCCATCATCAGCC:A" "10:119528844:T:TAATA"               "10:119528844:TAATA:T"              
+# [5] "10:119588327:T:TTTTC"               "10:119588327:TTTTC:T"  
+
+sjlife.bim <- sjlife.bim[!grepl("10:119614280|10:119485655|10:119588327:T:TTTTC|10:119528844:TAATA:T|10:119427920:A:ATCACTGCCATCATCAGCC", sjlife.bim$V2),]
+# dim(sjlife.bim)
+# [1] 2599    7
+
+wanted.snp.list <- paste0("chr", sjlife.bim$V2)
 
 write.table(as.data.frame(wanted.snp.list), "Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64/samplesnp.list", quote = F, row.names = F, col.names = F)
 
+# Merge sjlife bim with summary stat
+gwas.dat <- cbind.data.frame(sjlife.bim, gwas.dat[match(sjlife.bim$KEY, gwas.dat$SNP),])
 
+sum(gwas.dat$V5 == gwas.dat$A1)
+# 2599
+sum(gwas.dat$V6 == gwas.dat$A2)
+# 2599
 
-
-freq.dat <- read.delim("Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/sjlife.freq.out.frq_edited1", sep = "\t")
+freq.dat <- read.delim("Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64/sjlife.freq.out.frq_edited1", sep = "\t")
 dim(freq.dat)
 freq.dat
 
 
+# freq.dat$KEY <- vapply(strsplit(freq.dat$SNP, ':'), function(x)  paste(x[seq.int(2)], collapse=':'), character(1L))
+# freq.dat$KEY <- paste(freq.dat$KEY, freq.dat$A2, freq.dat$A1, sep = ":")
 
 
-freq.dat$KEY <- vapply(strsplit(freq.dat$SNP, ':'), function(x)  paste(x[seq.int(2)], collapse=':'), character(1L))
-freq.dat$KEY <- paste(freq.dat$KEY, freq.dat$A2, freq.dat$A1, sep = ":")
+## Add MAF
+sum(gwas.dat$V2 %in% freq.dat$SNP)
+# 2599
 
-
-
-sum(gwas.dat$SNP %in% freq.dat$KEY)
-# 2601
-
-gwas.dat$maf_original <- freq.dat$MAF[match(gwas.dat$SNP, freq.dat$KEY)]
-
-
-# ## SNP ID should be same as plink, so keeping the order of alleles accordingly
-# gwas.dat$rsid <- gwas.dat$KEY1
-# gwas.dat$rsid[is.na(gwas.dat$maf)] <- gwas.dat$KEY2[is.na(gwas.dat$maf)]
-# 
-# gwas.dat$allele_swapped <- ifelse(is.na(gwas.dat$maf), "Y", "N")
+gwas.dat$maf <- freq.dat$MAF[match(gwas.dat$V2, freq.dat$SNP)]
 
 
 
-# # Now adding maf of SNPs with swapped alleles
-# gwas.dat$maf[gwas.dat$allele_swapped == "Y"] <- 1-gwas.dat$maf_swapped[gwas.dat$allele_swapped == "Y"]
-# sum(is.na(gwas.dat$maf))
-# # 0
-
-gwas.dat <- cbind.data.frame(rsid=gwas.dat$SNP, chromosome=gwas.dat$CHR, position=gwas.dat$BP, 
-                             allele1 = gwas.dat$A1, allele2 = gwas.dat$A2, maf = gwas.dat$maf, OR_95percent_CI_SJLIFE = gwas.dat$OR_95percent_CI_SJLIFE, maf_original = gwas.dat$maf_original)
+gwas.dat <- cbind.data.frame(rsid=gwas.dat$V2, SNP=gwas.dat$SNP, chromosome=gwas.dat$CHR, position=gwas.dat$BP, 
+                             allele1 = gwas.dat$A1, allele2 = gwas.dat$A2, maf = gwas.dat$maf, OR_95percent_CI_SJLIFE = gwas.dat$OR_95percent_CI_SJLIFE)
 
 gwas.dat$OR <- as.numeric(sapply(strsplit(gwas.dat$OR_95percent_CI_SJLIFE, " "), `[`, 1))
 
@@ -73,28 +103,39 @@ gwas.dat$upperCI.beta <- log(gwas.dat$upperCI)
 gwas.dat$beta.SE <- (gwas.dat$upperCI.beta - gwas.dat$beta)/1.96
 
 
+
+# swap allele for this variant and change the sign of beta 
+gwas.dat$rsid == "10:119427920:ATCACTGCCATCATCAGCC:A"
+
+gwas.dat[gwas.dat$rsid == "10:119427920:ATCACTGCCATCATCAGCC:A", c("allele1", "allele2")] <- gwas.dat[gwas.dat$rsid == "10:119427920:ATCACTGCCATCATCAGCC:A", c("allele2", "allele1")]
+
+# Also change the sign of beta
+gwas.dat$beta[gwas.dat$rsid == "10:119427920:ATCACTGCCATCATCAGCC:A"]  <- (-1 * gwas.dat$beta[gwas.dat$rsid == "10:119427920:ATCACTGCCATCATCAGCC:A"])
+
+
+# Now, finally flip the allele and change the sign of beta where maf > 0.5
+gwas.dat[gwas.dat$maf > 0.5 , c("allele1", "allele2")] <- gwas.dat[gwas.dat$maf > 0.5, c("allele2", "allele1")] # swap alleles
+gwas.dat$beta[gwas.dat$maf > 0.5] <- (-1 * gwas.dat$beta[gwas.dat$maf > 0.5]) # Change the sign of beta
+gwas.dat$maf[gwas.dat$maf > 0.5] <- 1-gwas.dat$maf[gwas.dat$maf > 0.5] # Finally, change the maf values
+
+
 ## Now Keep only the the required columns
 gwas.dat <- cbind.data.frame(rsid=gwas.dat$rsid, chromosome=gwas.dat$chromosome, position=gwas.dat$position, 
                              allele1 = gwas.dat$allele1, allele2 = gwas.dat$allele2, maf = gwas.dat$maf, beta = gwas.dat$beta, se = gwas.dat$beta.SE)
 
 
-gwas.dat$rsid <- paste0("chr",gwas.dat$rsid)
+# Now sort this summary stat in the same order as Plink
+gwas.dat$rsid <- paste0("chr", gwas.dat$rsid)
+samplesnp.dat.bim <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64/samplesnp.dat.bim", sep = "\t", header = F)
 
+sum(samplesnp.dat.bim$V2 %in% gwas.dat$rsid)
+# 2599
+gwas.dat <- gwas.dat[match(samplesnp.dat.bim$V2, gwas.dat$rsid),]
+table(gwas.dat$rsid == samplesnp.dat.bim$V2)
+# TRUE 
+# 2599 
 
-## write SNP list to get LD-matrix
-# write.table(as.data.frame(paste(gwas.dat$rsid, gwas.dat$allele2, gwas.dat$allele1, sep = ":")), "Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64/samplesnp.list", quote = F, row.names = F, col.names = F)
-write.table(as.data.frame(gwas.dat$rsid), "Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64/samplesnp.list", quote = F, row.names = F, col.names = F)
-
-
-# Error : Expected a floating point value greater than 0.0 and smaller or equal than 0.5 in line 3 column 6 of file 'samplesnp.z'!
-gwas.dat.edited <- gwas.dat  
-gwas.dat.edited$maf
-
+# Write data.z file for FINEMAP
 write.table(gwas.dat, "Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64/samplesnp.z", quote = F, row.names = F, col.names = T, sep = " ")
 
 
-## Reorder LD file
-ld.file <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/FINEMAP/finemap_v1.4.1_x86_64/samplesnp_ld.ld", header = F)
-dim(ld.file)
-
-sum(ld.file$SNP_B %in% gwas.dat$rsid)
