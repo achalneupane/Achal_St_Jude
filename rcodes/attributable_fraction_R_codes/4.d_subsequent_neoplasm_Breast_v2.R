a41b21064c6c2380240dab573a3fc827eed3cd05
Neupane <achalneupane@gmail.com>
2022-08-16 16:22:09 -0500

    .

--- rcodes/attributable_fraction_R_codes/4.d_subsequent_neoplasm_Breast_v2.R
+++ rcodes/attributable_fraction_R_codes/4.d_subsequent_neoplasm_Breast_v2.R
@@ -102,9 +102,9 @@ summary(mod1.EUR)
 ## Attributable Fractions ##
 ############################
 
-# PRS 2015
+## --------------------------------------1. PRS 2015
 dat_all = PHENO.ANY_SN
-fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers + 
+fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_without_Zhaoming_vars_carriers + 
                 Mavaddat_2015_ER_POS_Breast_PRS.tertile.category +
                 Mavaddat_2015_ER_OVERALL_Breast_PRS.tertile.category +
                 Mavaddat_2015_ER_NEG_Breast_PRS.tertile.category +
@@ -115,9 +115,57 @@ fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers +
 
 summary(fit_all)
 
-# PRS 2019
+
+##########################
+## Get predicted values ##
+##########################
+dat_all$pred_all = predict(fit_all, newdat = dat_all, type = "response")
+
+###############
+## Treatment ##
+###############
+
+## Move relevant treatment exposures for everyone to no exposure
+dat_tx = dat_all
+dat_tx$maxchestrtdose.category = dat_tx$anthra_jco_dose_5.category = "None"
+dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
+
+## Attributable fraction calculation. First get the "predicted" number of SNs based on the model including all variables
+N_all = sum(dat_all$pred_all, na.rm = TRUE)
+N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
+af_by_tx = (N_all - N_no_tx) / N_all
+round(af_by_tx,3)
+# 0.542
+
+##########
+## P/LP ##
+##########
+## P/LP Zhaoming and Qin without Zhaoming
+dat_plp = dat_all
+dat_plp$Zhaoming_carriers = dat_plp$Qin_without_Zhaoming_vars_carriers = "N"
+
+dat_all$pred_no_plp = predict(fit_all, newdata = dat_plp, type = "response")
+N_no_plp = sum(dat_all$pred_no_plp, na.rm = TRUE)
+af_by_plp_Zhaoming = (N_all - N_no_plp) / N_all
+round(af_by_plp_Zhaoming,3)
+# 0.07
+
+#########
+## PRS ##
+#########
+dat_prs = dat_all
+dat_prs$Mavaddat_2015_ER_POS_Breast_PRS.tertile.category = dat_prs$Mavaddat_2015_ER_NEG_Breast_PRS.tertile.category = dat_prs$Mavaddat_2015_ER_OVERALL_Breast_PRS.tertile.category = "1st"
+
+dat_all$pred_no_Mavaddat_2015.tertile.category = predict(fit_all, newdata = dat_prs, type = "response")
+N_no_pred_no_Mavaddat_2015.tertile.category = sum(dat_all$pred_no_Mavaddat_2015.tertile.category, na.rm = TRUE)
+af_by_N_no_pred_no_Mavaddat_2015.tertile.category = (N_all - N_no_pred_no_Mavaddat_2015.tertile.category) / N_all
+round(af_by_N_no_pred_no_Mavaddat_2015.tertile.category, 3)
+# 0.388
+
+## --------------------------------------2. PRS 2019
+
 dat_all = PHENO.ANY_SN
-fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers + 
+fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_without_Zhaoming_vars_carriers + 
                 Mavaddat_2019_ER_POS_Breast_PRS.tertile.category +
                 Mavaddat_2019_ER_OVERALL_Breast_PRS.tertile.category +
                 Mavaddat_2019_ER_NEG_Breast_PRS.tertile.category +
@@ -128,9 +176,57 @@ fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers +
 
 summary(fit_all)
 
-# PRSWEB
+##########################
+## Get predicted values ##
+##########################
+dat_all$pred_all = predict(fit_all, newdat = dat_all, type = "response")
+
+###############
+## Treatment ##
+###############
+
+## Move relevant treatment exposures for everyone to no exposure
+dat_tx = dat_all
+dat_tx$maxchestrtdose.category = dat_tx$anthra_jco_dose_5.category = "None"
+dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
+
+## Attributable fraction calculation. First get the "predicted" number of SNs based on the model including all variables
+N_all = sum(dat_all$pred_all, na.rm = TRUE)
+N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
+af_by_tx = (N_all - N_no_tx) / N_all
+round(af_by_tx, 3)
+# 0.546
+
+##########
+## P/LP ##
+##########
+## P/LP Zhaoming and Qin without Zhaoming
+dat_plp = dat_all
+dat_plp$Zhaoming_carriers = dat_plp$Qin_without_Zhaoming_vars_carriers = "N"
+
+dat_all$pred_no_plp = predict(fit_all, newdata = dat_plp, type = "response")
+N_no_plp = sum(dat_all$pred_no_plp, na.rm = TRUE)
+af_by_plp_Zhaoming = (N_all - N_no_plp) / N_all
+round(af_by_plp_Zhaoming, 3)
+# 0.065
+
+#########
+## PRS ##
+#########
+dat_prs = dat_all
+dat_prs$Mavaddat_2019_ER_POS_Breast_PRS.tertile.category = dat_prs$Mavaddat_2019_ER_NEG_Breast_PRS.tertile.category = dat_prs$Mavaddat_2019_ER_OVERALL_Breast_PRS.tertile.category = "1st"
+
+dat_all$pred_no_Mavaddat_2015.tertile.category = predict(fit_all, newdata = dat_prs, type = "response")
+N_no_pred_no_Mavaddat_2015.tertile.category = sum(dat_all$pred_no_Mavaddat_2015.tertile.category, na.rm = TRUE)
+af_by_N_no_pred_no_Mavaddat_2015.tertile.category = (N_all - N_no_pred_no_Mavaddat_2015.tertile.category) / N_all
+round(af_by_N_no_pred_no_Mavaddat_2015.tertile.category, 3)
+# 0.557
+
+
+
+## --------------------------------------3. PRSWEB
 dat_all = PHENO.ANY_SN
-fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers + 
+fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_without_Zhaoming_vars_carriers + 
                 MichiganWeb_ER_NEG_Breast_PRS.tertile.category +
                 MichiganWeb_ER_OVERALL_Breast_PRS.tertile.category +
                 MichiganWeb_ER_POS_Breast_PRS.tertile.category +
@@ -141,10 +237,57 @@ fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers +
 
 summary(fit_all)
 
+##########################
+## Get predicted values ##
+##########################
 
-# Khera et al
+dat_all$pred_all = predict(fit_all, newdat = dat_all, type = "response")
+
+###############
+## Treatment ##
+###############
+
+## Move relevant treatment exposures for everyone to no exposure
+dat_tx = dat_all
+dat_tx$maxchestrtdose.category = dat_tx$anthra_jco_dose_5.category = "None"
+dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
+
+## Attributable fraction calculation. First get the "predicted" number of SNs based on the model including all variables
+N_all = sum(dat_all$pred_all, na.rm = TRUE)
+N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
+af_by_tx = (N_all - N_no_tx) / N_all
+round(af_by_tx, 3)
+# 0.545
+
+##########
+## P/LP ##
+##########
+## P/LP Zhaoming and Qin without Zhaoming
+dat_plp = dat_all
+dat_plp$Zhaoming_carriers = dat_plp$Qin_without_Zhaoming_vars_carriers = "N"
+
+dat_all$pred_no_plp = predict(fit_all, newdata = dat_plp, type = "response")
+N_no_plp = sum(dat_all$pred_no_plp, na.rm = TRUE)
+af_by_plp_Zhaoming = (N_all - N_no_plp) / N_all
+round(af_by_plp_Zhaoming, 3)
+# 0.067
+
+#########
+## PRS ##
+#########
+dat_prs = dat_all
+dat_prs$MichiganWeb_ER_NEG_Breast_PRS.tertile.category = dat_prs$MichiganWeb_ER_POS_Breast_PRS.tertile.category = dat_prs$MichiganWeb_ER_OVERALL_Breast_PRS.tertile.category = "1st"
+
+dat_all$pred_no_Mavaddat_2015.tertile.category = predict(fit_all, newdata = dat_prs, type = "response")
+N_no_pred_no_Mavaddat_2015.tertile.category = sum(dat_all$pred_no_Mavaddat_2015.tertile.category, na.rm = TRUE)
+af_by_N_no_pred_no_Mavaddat_2015.tertile.category = (N_all - N_no_pred_no_Mavaddat_2015.tertile.category) / N_all
+round(af_by_N_no_pred_no_Mavaddat_2015.tertile.category, 3)
+# 0.406
+
+
+## --------------------------------------4. Khera
 dat_all = PHENO.ANY_SN
-fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers + 
+fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_without_Zhaoming_vars_carriers + 
                 Khera_2018_Breast_PRS.tertile.category +
                 AGE_AT_LAST_CONTACT.cs1 + AGE_AT_LAST_CONTACT.cs2+ 
                 AGE_AT_LAST_CONTACT.cs3 + AGE_AT_LAST_CONTACT.cs4 + AGE_AT_DIAGNOSIS +
@@ -153,130 +296,51 @@ fit_all = glm(formula = BREASTcancer ~ Zhaoming_carriers + Qin_carriers +
 
 summary(fit_all)
 
+##########################
+## Get predicted values ##
+##########################
 
-# Get predicted values
 dat_all$pred_all = predict(fit_all, newdat = dat_all, type = "response")
+
+###############
+## Treatment ##
+###############
 
 ## Move relevant treatment exposures for everyone to no exposure
 dat_tx = dat_all
 dat_tx$maxchestrtdose.category = dat_tx$anthra_jco_dose_5.category = "None"
 dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
 
-## Attributable fraction calculation
-# First get the "predicted" number of SNs
-# Based on the model including all variables
+## Attributable fraction calculation. First get the "predicted" number of SNs based on the model including all variables
 N_all = sum(dat_all$pred_all, na.rm = TRUE)
 N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
 af_by_tx = (N_all - N_no_tx) / N_all
-print(af_by_tx)
-# 0.5470882
-# 0.5516615
-# 0.5523445
+round(af_by_tx, 3)
+# 0.539
 
-## maxchestrtdose.category
-dat_tx = dat_all
-dat_tx$maxchestrtdose.category = "None"
-dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
-
-N_all = sum(dat_all$pred_all, na.rm = TRUE)
-N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
-af_by_tx = (N_all - N_no_tx) / N_all
-print(af_by_tx)
-# 0.4945335
-# 0.4954146
-# 0.5004813
-
-## anthra_jco_dose_5.category
-dat_tx = dat_all
-dat_tx$anthra_jco_dose_5.category = "None"
-dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
-
-N_all = sum(dat_all$pred_all, na.rm = TRUE)
-N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
-af_by_tx = (N_all - N_no_tx) / N_all
-print(af_by_tx)
-# 0.100922
-# 0.1084345
-# 0.09832719
-
-## P/LP Zhaoming
+##########
+## P/LP ##
+##########
+## P/LP Zhaoming and Qin without Zhaoming
 dat_plp = dat_all
-dat_plp$Zhaoming_carriers = "N"
+dat_plp$Zhaoming_carriers = dat_plp$Qin_without_Zhaoming_vars_carriers = "N"
 
 dat_all$pred_no_plp = predict(fit_all, newdata = dat_plp, type = "response")
 N_no_plp = sum(dat_all$pred_no_plp, na.rm = TRUE)
 af_by_plp_Zhaoming = (N_all - N_no_plp) / N_all
-print(af_by_plp_Zhaoming)
-# 0.05583012
-# 0.05341221
-# 0.05524048
-
-## P/LP Qin
-dat_plp = dat_all
-dat_plp$Qin_carriers = "N"
-
-dat_all$pred_no_plp = predict(fit_all, newdata = dat_plp, type = "response")
-N_no_plp = sum(dat_all$pred_no_plp, na.rm = TRUE)
-af_by_plp_Qin = (N_all - N_no_plp) / N_all
-print(af_by_plp_Qin)
-# 0.0008419519
-# 0.0008096127
-# -0.001769915
-
-## H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts
-dat_plp = dat_all
-dat_plp$H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts  = 0
-
-dat_all$H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts = predict(fit_all, newdata = dat_plp, type = "response")
-N_no_pred_H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts = sum(dat_all$H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts, na.rm = TRUE)
-af_by_N_no_pred_H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts = (N_all - N_no_pred_H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts) / N_all
-print(af_by_N_no_pred_H.C.Clin.LoF.MetaSVM.WO.Zhao.Qin_variants.Non.Ref.Counts)
-# -24.57465
-# -23.21813
-# -21.60368
-
-## All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts
-dat_plp = dat_all
-dat_plp$All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts  = 0
-
-dat_all$All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts = predict(fit_all, newdata = dat_plp, type = "response")
-N_no_pred_All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts = sum(dat_all$All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts, na.rm = TRUE)
-af_by_N_no_pred_All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts = (N_all - N_no_pred_All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts) / N_all
-print(af_by_N_no_pred_All.P.LP.clinvars.LoF.MetaSVM.WO.Prior.vars.Non.Ref.Counts)
-# -4.260216
-# -2.897749
-# -6.729559
+round(af_by_plp_Zhaoming, 3)
+# 0.067
 
 #########
 ## PRS ##
 #########
-# Mavaddat_2015_Breast
 dat_prs = dat_all
-dat_prs$Mavaddat_2015_ER_POS_Breast_PRS.tertile.category = dat_prs$Mavaddat_2015_ER_NEG_Breast_PRS.tertile.category = dat_prs$Mavaddat_2015_ER_OVERALL_Breast_PRS.tertile.category = "1st"
+dat_prs$Khera_2018_Breast_PRS.tertile.category = "1st"
 
 dat_all$pred_no_Mavaddat_2015.tertile.category = predict(fit_all, newdata = dat_prs, type = "response")
 N_no_pred_no_Mavaddat_2015.tertile.category = sum(dat_all$pred_no_Mavaddat_2015.tertile.category, na.rm = TRUE)
 af_by_N_no_pred_no_Mavaddat_2015.tertile.category = (N_all - N_no_pred_no_Mavaddat_2015.tertile.category) / N_all
-print(af_by_N_no_pred_no_Mavaddat_2015.tertile.category)
-# -2.472324
+round(af_by_N_no_pred_no_Mavaddat_2015.tertile.category, 3)
+# 0.275
 
 
-# Mavaddat_2019_Breast
-dat_prs = dat_all
-dat_prs$Mavaddat_2019_ER_POS_Breast_PRS.tertile.category = dat_prs$Mavaddat_2019_ER_NEG_Breast_PRS.tertile.category = dat_prs$Mavaddat_2019_ER_OVERALL_Breast_PRS.tertile.category = "1st"
-
-dat_all$pred_no_Mavaddat_2019.tertile.category = predict(fit_all, newdata = dat_prs, type = "response")
-N_no_pred_no_Mavaddat_2019.tertile.category = sum(dat_all$pred_no_Mavaddat_2019.tertile.category, na.rm = TRUE)
-af_by_N_no_pred_no_Mavaddat_2019.tertile.category = (N_all - N_no_pred_no_Mavaddat_2019.tertile.category) / N_all
-print(af_by_N_no_pred_no_Mavaddat_2019.tertile.category)
-# -1.016479
-
-# PRSWEB
-dat_prs = dat_all
-dat_prs$MichiganWeb_ER_NEG_Breast_PRS.tertile.category = dat_prs$MichiganWeb_ER_OVERALL_Breast_PRS.tertile.category = dat_prs$MichiganWeb_ER_POS_Breast_PRS.tertile.category = "1st"
-
-dat_all$pred_no_MichiganWeb.tertile.category = predict(fit_all, newdata = dat_prs, type = "response")
-N_no_pred_no_MichiganWeb.tertile.category = sum(dat_all$pred_no_MichiganWeb.tertile.category, na.rm = TRUE)
-af_by_N_no_pred_no_MichiganWeb.tertile.category = (N_all - N_no_pred_no_MichiganWeb.tertile.category) / N_all
-print(af_by_N_no_pred_no_MichiganWeb.tertile.category)
-# -4.377463
