4f6e7f1e7abc011fa03f0fbde50322f3bdf02e26
Neupane <achalneupane@gmail.com>
2022-09-12 10:10:48 -0500

    .

---
+++ bash/diabetes_2022/skat_burden_test.sh
@@ -0,0 +1,4 @@
+## Merge all plink files
+# plink --bfile MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr1.PASS.decomposed_geno.0.1_hwe.1e-10 --make-bed --merge-list merge_all.list --keep-allele-order --out MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chrALL.PASS.decomposed_geno.0.1_hwe.1e-10
+
+
