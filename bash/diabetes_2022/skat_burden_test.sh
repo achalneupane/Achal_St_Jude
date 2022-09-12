df1380c1859e35ac8559d67330a550175c098b54
Neupane <achalneupane@gmail.com>
2022-09-12 16:38:11 -0500

    .

--- bash/diabetes_2022/skat_burden_test.sh
+++ bash/diabetes_2022/skat_burden_test.sh
@@ -2,3 +2,13 @@
 # plink --bfile MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chr1.PASS.decomposed_geno.0.1_hwe.1e-10 --make-bed --merge-list merge_all.list --keep-allele-order --out MERGED.SJLIFE.1.2.GATKv3.4.VQSR.sjlid_chrALL.PASS.decomposed_geno.0.1_hwe.1e-10
 
 
+## Extract gnomAD with maf < 0.01 lines
+head -1 /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/annovar/ANNOVAR_MERGED.SJLIFE.1.2.GATKv3.4.VQSR.chr5.preQC_biallelic_renamed_ID_edited.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155.vcf.hg38_multianno.txt | sed 's/\t/\n/g' | nl
+# head -1 /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/annovar/ANNOVAR_MERGED.SJLIFE.1.2.GATKv3.4.VQSR.chr5.preQC_biallelic_renamed_ID_edited.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155.vcf.hg38_multianno.txt > /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/diabetes/gnomAD_maf_lt_0.01.txt
+
+for i in {1..22}; do
+CHROM="chr${i}"
+echo "Doing ${CHROM}"
+head -1 /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/annovar/ANNOVAR_MERGED.SJLIFE.1.2.GATKv3.4.VQSR.chr5.preQC_biallelic_renamed_ID_edited.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155.vcf.hg38_multianno.txt| awk '{print $178"\t"$179"\t"$181"\t"$182"\t"$38"\t"$39"\t"$44}' > /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/diabetes/gnomAD_maf_lt_0.01_${CHROM}.txt	
+awk -F'\t' '{if($38 != "." && $38 < 0.01) print $178"\t"$179"\t"$181"\t"$182"\t"$38"\t"$39"\t"$44}' "/research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/annovar/ANNOVAR_MERGED.SJLIFE.1.2.GATKv3.4.VQSR.${CHROM}.preQC_biallelic_renamed_ID_edited.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155.vcf.hg38_multianno.txt" >> /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/diabetes/gnomAD_maf_lt_0.01_${CHROM}.txt
+done
--- rcodes/diabetes_2022/diabetes_analysis.r
+++ rcodes/diabetes_2022/diabetes_analysis.r
@@ -1,5 +1,7 @@
-# load("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/snpEff/SNPEFF_clinvar_metaSVM_LoF_from_R_filtering_process_PreQC_VCF.RData")
-load("/research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/snpEff/SNPEFF_clinvar_metaSVM_LoF_from_R_filtering_process_PreQC_VCF.RData")
+# Main folder: /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/diabetes
+
+load("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/snpEff/SNPEFF_clinvar_metaSVM_LoF_from_R_filtering_process_PreQC_VCF.RData")
+# load("/research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/snpEff/SNPEFF_clinvar_metaSVM_LoF_from_R_filtering_process_PreQC_VCF.RData")
 
 
 
@@ -24,17 +26,19 @@ Predicted.vars.inVCF.Unique <-  rbind.data.frame(CLINVAR.unique, LOF.keep)
 
 FINAL <- {}
 
-for (CHR in 1:22){
-print(paste0("DOING chr", CHR))
+CHROM <- 1:22
+for (i in 1:length(CHROM)){
+print(paste0("DOING chr", CHROM[i]))
 ## Extract gnomAD MAF
-# annovar.chr <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/MERGED_sjlife1_2_PreQC/cleaned/annotation/annovar/ANNOVAR_MERGED.SJLIFE.1.2.GATKv3.4.VQSR.chr7.preQC_biallelic_renamed_ID_edited.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155.vcf.hg38_multianno.txt", sep = "\t", stringsAsFactors = F)
-annovar.chr <- read.delim(paste0("/research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/MERGED_sjlife1_2_PreQC/cleaned/annotation/annovar/ANNOVAR_MERGED.SJLIFE.1.2.GATKv3.4.VQSR.chr",CHR,".preQC_biallelic_renamed_ID_edited.vcf-annot-snpeff-dbnsfp-ExAC.0.3-clinvar.GRCh38.vcf.dbSNP155.vcf.hg38_multianno.txt"), sep = "\t", stringsAsFactors = F, header = T)
+annovar.chr <- read.delim(paste0("Z:/ResearchHome/Groups/sapkogrp/projects//Genomics/common/diabetes/gnomAD_maf_lt_0.01_chr",CHROM[i],".txt"), sep = "\t", header = T)
+# annovar.chr <- read.delim(paste0("/research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/diabetes/gnomAD_maf_lt_0.01_chr",CHROM[i],".txt"), sep = "\t", stringsAsFactors = F, header = T)
 
 annovar.chr$KEY <- paste(annovar.chr$Otherinfo4, annovar.chr$Otherinfo5, annovar.chr$Otherinfo7, annovar.chr$Otherinfo8, sep = ":")
-# annovar.chr$KEY.chr.pos <- paste(annovar.chr$Chr, annovar.chr$Start, sep = ":")
+# annovar.chr$KEY.chr.pos <- paste(annovar.chr$Otherinfo4, annovar.chr$Otherinfo5, sep = ":")
 # Predicted.vars.inVCF.Unique$KEY.chr.pos <- paste(Predicted.vars.inVCF.Unique$CHROM, Predicted.vars.inVCF.Unique$POS, sep = ":")
 
 sum(Predicted.vars.inVCF.Unique$KEY %in% annovar.chr$KEY)
+# sum(Predicted.vars.inVCF.Unique$KEY.chr.pos %in% annovar.chr$KEY.chr.pos)
 
 # gnomAD ALL
 Predicted.vars.inVCF.Unique$gnomAD_genome_ALL <- annovar.chr$gnomAD_genome_ALL [match(Predicted.vars.inVCF.Unique$KEY, annovar.chr$KEY)]
@@ -45,9 +49,16 @@ wanted.vars.all <- Predicted.vars.inVCF.Unique[!is.na(Predicted.vars.inVCF.Uniqu
 wanted.vars.all <- wanted.vars.all[wanted.vars.all$gnomAD_genome_ALL < 0.01,]
 
 FINAL <- rbind.data.frame(FINAL, wanted.vars.all)
-print(paste0("NROWS of FINAL", nrow(FINAL)))
+print(paste0("NROWS of FINAL ", nrow(FINAL)))
 }
 
+# Keep the unique variants only
+FINAL <- FINAL[!duplicated(FINAL$KEY),]
+
+rm (annovar.chr)
+
+
+write.table("Z:/ResearchHome/Groups/sapkogrp/projects//Genomics/common/diabetes/extract_variants_gnomAD_ALL_NFE_lt_0.01")
 
 # gnomAD NFE
 Predicted.vars.inVCF.Unique$gnomAD_genome_NFE <- annovar.chr$gnomAD_genome_NFE [match(Predicted.vars.inVCF.Unique$KEY, annovar.chr$KEY)]
