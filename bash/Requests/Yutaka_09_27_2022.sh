1da4b847998da0becc9bc175ced8e1a5b23d6de1
Neupane <achalneupane@gmail.com>
2022-09-30 14:40:01 -0500

    .

--- bash/Requests/Yutaka_09_27_2022.sh
+++ bash/Requests/Yutaka_09_27_2022.sh
@@ -11,6 +11,10 @@ rs7853758
 rs4982753
 rs4149178
 
+##################
+## ccss_exp_WGS ##
+##################
+
 # GrCh38
 cat <<\EoF > extract_vars_GRCH38.bed
 #chrom chromStart chromEnd name ref alts
@@ -24,18 +28,10 @@ chr18 37497064 37497065 rs1786814 G A,C,
 chr21 36146407 36146408 rs1056892 G A,
 EoF
 
-# GRCh37
-cat <<\EoF > extract_vars_GRCH37.bed
-#chrom chromStart chromEnd name ref alts
-chr2 234602276 234602277 rs17863783 G T,
-chr6 43272187 43272188 rs4149178 A G,
-chr9 86900925 86900926 rs7853758 G A,
-chr12 53605544 53605545 rs2229774 G A,
-chr14 23814568 23814569 rs4982753 C A,T,
-chr16 69143576 69143577 rs2232228 A C,G,
-chr18 35077027 35077028 rs1786814 G A,C,
-chr21 37518705 37518706 rs1056892 G A,
-EoF
+samples	chr2:233693631:G:T	chr6:43304450:A:G	chr9:84286011:G:A	chr12:53211761:G:A	chr14:23345360:C:T	chr16:69109674:A:G	chr18:37497065:G:A	chr21:36146408:G:A
+samples	rs17863783	rs4149178	rs7853758	rs2229774	rs4982753	rs2232228	rs1786814	rs1056892
+
+
 
 cd /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022
 ln -s ../CCSS_exp_biallelic*vcf.gz* .
@@ -43,15 +39,13 @@ ln -s ../CCSS_exp_biallelic*vcf.gz* .
 module load bcftools/1.9
 module load plink/1.90b
 
-cat extract_vars_GRCH38.txt |sed 's/^[ \t]*//;s/[ \t]*$//' > extract_vars_GRCH38_edited.txt
-
 IFS=$'\n' # set IFS
 for line in $(cat extract_vars_GRCH38.bed| sed '1d'); do
 VAR="$(echo ${line}| awk '{print $1":"$2"-"$3}')"
 CHR="$(echo $VAR |awk -F':' '{print $1}')"
 echo "Doing ${VAR}"
 bcftools view /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/CCSS_exp_biallelic_${CHR}_ID_edited.vcf.gz ${VAR} > /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.vcf.gz
-bcftools query -f '%CHROM:%POS:%REF:%ALT[\n%SAMPLE=%GT]\n' /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.vcf.gz >> /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.geno.txt
+# bcftools query -f '%CHROM:%POS:%REF:%ALT[\n%SAMPLE=%GT]\n' /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.vcf.gz >> /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.geno.txt
 plink --vcf /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.vcf.gz --double-id --vcf-half-call m --keep-allele-order --make-bed --out /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/yutaka_${VAR} 2>&1 | tee -a extract_plink_all.log
 done
 
@@ -64,3 +58,50 @@ plink --bfile yutaka_chr2:233693630-233693631 --merge-list merge_list.list --kee
 
 plink --bfile merged.dat.yutaka --recodeA --out merged.dat.yutaka_recodeA 
 
+##################
+## ccss_org_hrc ##
+##################
+
+# GRCh37
+cat <<\EoF > extract_vars_GRCH37.bed
+#chrom chromStart chromEnd name ref alts
+2 234602276 234602277 rs17863783 G T,
+6 43272187 43272188 rs4149178 A G,
+9 86900925 86900926 rs7853758 G A,
+12 53605544 53605545 rs2229774 G A,
+14 23814568 23814569 rs4982753 C A,T,
+16 69143576 69143577 rs2232228 A C,G,
+18 35077027 35077028 rs1786814 G A,C,
+21 37518705 37518706 rs1056892 G A,
+EoF
+
+cd /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022
+ln -s ../CCSS_exp_biallelic*vcf.gz* .
+
+mkdir plink_data
+
+module load bcftools/1.9
+module load plink/1.90b
+
+# cat extract_vars_GRCH37.txt |sed 's/^[ \t]*//;s/[ \t]*$//' > extract_vars_GRCH37_edited.txt
+
+IFS=$'\n' # set IFS
+for line in $(cat extract_vars_GRCH37.bed| sed '1d'); do
+VAR="$(echo ${line}| awk '{print $1":"$2"-"$3}')"
+CHR="$(echo $VAR |awk -F':' '{print $1}')"
+echo "Doing ${VAR}"
+bcftools view /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/chr${CHR}.dose.vcf.gz ${VAR} > /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.vcf.gz
+# bcftools query -f '%CHROM:%POS:%REF:%ALT[\n%SAMPLE=%GT]\n' /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.vcf.gz >> /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.geno.txt
+# annotate VCF
+bcftools annotate --set-id '%CHROM\:%POS\:%REF\:%FIRST_ALT' /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}.vcf.gz -Oz -o /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}_edited.vcf.gz
+plink --vcf /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}_edited.vcf.gz --double-id --vcf-half-call m --keep-allele-order --make-bed --out /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/yutaka_${VAR}_edited 2>&1 | tee -a extract_plink_all.log
+done
+
+# paste *.geno.txt | sed 's/^[ \t]*//;s/[ \t]*$//' > all_genotypes_from_ccss_WGS_for_Yutaka.txt
+
+cd /research_jude/rgs01_jude/groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data
+ls *.bim| sort -V | sed 's/\.bim//g'|sed -n '1d;p' > merge_list.list
+
+plink --bfile yutaka_2:234602276-234602277_edited --merge-list merge_list.list --keep-allele-order --out merged.dat.yutaka
+
+plink --bfile merged.dat.yutaka --recodeA --out merged.dat.yutaka_recodeA 
--- rcodes/requests_from_collaborators/Yutaka_email_request_for_variant_extraction.R
+++ rcodes/requests_from_collaborators/Yutaka_email_request_for_variant_extraction.R
@@ -1,9 +1,14 @@
+##################
+## CCSS_exp_WGS ##
+##################
+
 setwd("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/ccss_exp_wgs/preQC_VCF_per_chromosome/yutaka_request_09_27_2022/plink_data/")
 
+library(dplyr)
 
 # df <- read.delim("all_genotypes_from_ccss_WGS_for_Yutaka.txt", sep = "\t", header = T)
 
-raw <- read.delim("merged.dat.yutaka_recodeA.raw", sep = " ", header = T)
+raw <- read.delim("merged.dat.yutaka_recodeA.raw", sep = " ", header = T, check.names = F)
 dim(raw)
 head(raw)
 
@@ -24,8 +29,8 @@ for (i in 1:ncol(raw)){
   # i=1
 df.list.tmp <- raw[i]
 
-REF = unlist(strsplit(colnames(df.list.tmp), "\\."))[3]
-ALT = unlist(strsplit(colnames(df.list.tmp), "\\."))[4]
+REF = unlist(strsplit(colnames(df.list.tmp), "\\:"))[3]
+ALT = unlist(strsplit(colnames(df.list.tmp), "\\:"))[4]
 
 df.list.tmp$genotype <- as.character(df.list.tmp[,1])
 df.list.tmp$genotype <- gsub("0", paste0(REF,REF), df.list.tmp$genotype)
@@ -33,8 +38,61 @@ df.list.tmp$genotype <- gsub("1", paste0(REF,ALT), df.list.tmp$genotype)
 df.list.tmp$genotype <- gsub("2", paste0(ALT,ALT), df.list.tmp$genotype)
 df.list.tmp$samples <- row.names(df.list.tmp)
 colnames(df.list.tmp)
-df.list.tmp <- df.list.tmp[, c("samples", "genotype")]
+df.list.tmp[1] <- df.list.tmp$genotype
+df.list.tmp <- df.list.tmp[-2]
 df.list[[i]] <- df.list.tmp
 }
 
-cc <- bind_rows(df.list, .id = "samples")
+cc <- Reduce(function(...) merge(..., by= "samples", all.x=T), df.list)
+
+write.table(cc, "CCSS_exp_WGS_yutaka_genotype.txt", sep = "\t", col.names = T, quote = F, row.names = F)
+
+##################
+## ccss_org_hrc ##
+##################
+
+setwd("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/ccss_org_hrc/ccss_org_hrc_vcf_GRCh38/yutaka_request_09_27_2022/plink_data/")
+
+library(dplyr)
+
+# df <- read.delim("all_genotypes_from_ccss_WGS_for_Yutaka.txt", sep = "\t", header = T)
+
+raw <- read.delim("merged.dat.yutaka_recodeA.raw", sep = " ", header = T, check.names = F)
+dim(raw)
+head(raw)
+
+HEADER = sub(pattern="_[T,A,G,C,*]+",replacement="",colnames(raw))
+HEADER = gsub(pattern=";rs\\d+",replacement="",HEADER)
+colnames(raw) = HEADER
+
+# exclude chr9.84286010.A.G
+raw <- raw[!grepl("FID|PAT|MAT|SEX|PHENOTYPE", colnames(raw))]
+colnames(raw) # check this with the .bim REF and NON-REFERENCE alleles
+
+
+rownames(raw) <- raw$IID
+raw <- raw[!grepl("IID", colnames(raw))]
+
+df.list <- list()
+for (i in 1:ncol(raw)){
+  # i=1
+  df.list.tmp <- raw[i]
+  
+  REF = unlist(strsplit(colnames(df.list.tmp), "\\:"))[3]
+  ALT = unlist(strsplit(colnames(df.list.tmp), "\\:"))[4]
+  
+  df.list.tmp$genotype <- as.character(df.list.tmp[,1])
+  df.list.tmp$genotype <- gsub("0", paste0(REF,REF), df.list.tmp$genotype)
+  df.list.tmp$genotype <- gsub("1", paste0(REF,ALT), df.list.tmp$genotype)
+  df.list.tmp$genotype <- gsub("2", paste0(ALT,ALT), df.list.tmp$genotype)
+  df.list.tmp$samples <- row.names(df.list.tmp)
+  colnames(df.list.tmp)
+  df.list.tmp[1] <- df.list.tmp$genotype
+  df.list.tmp <- df.list.tmp[-2]
+  df.list[[i]] <- df.list.tmp
+}
+
+cc <- Reduce(function(...) merge(..., by= "samples", all.x=T), df.list)
+
+write.table(cc, "CCSS_org_hrc_yutaka_genotype.txt", sep = "\t", col.names = T, quote = F, row.names = F)
+
