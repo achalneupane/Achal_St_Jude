33de83e45b9481bb48bb344edf4918d6f254bffa
Neupane <achalneupane@gmail.com>
2022-10-17 11:26:04 -0500

    .

--- bash/count_fastq_lines.sh
+++ bash/count_fastq_lines.sh
@@ -19,22 +19,11 @@ for file1 in $(ls ./*/*_1.fq.gz| sort -V); do
 file2="$(echo $file1 | sed 's/_1.fq.gz/_2.fq.gz/g')"
 file1_lines="$(zcat ${file1}| wc -l)"
 file2_lines="$(zcat ${file2}| wc -l)"
-echo "${file1} has ${file1_lines} lines; ${file2} has ${file2_lines} lines " >> /research_jude/rgs01_jude/groups/sapkogrp/projects/CAB/common/WGS_Northwestern/read_counts_HLHS.txt
+echo "${file1} has ${file1_lines} lines; ${file2} has ${file2_lines} lines " >> /research_jude/rgs01_jude/groups/sapkogrp/projects/CAB/common/WGS_Northwestern/read_counts_Ashley.txt
 done
 
 
-./GW3-7/V350082588_L03_B5GHUMqzinRABCA-570_1.fq.gz has 87524113 lines; ./GW3-7/V350082588_L03_B5GHUMqzinRABCA-570_2.fq.gz has 167690820 lines
-./GW21-6/V350095281_L01_B5GHUMqzinRABEA-582_1.fq.gz has 277092808 lines; ./GW21-6/V350095281_L01_B5GHUMqzinRABEA-582_2.fq.gz has 99973927 lines
-./GW22-36/V350095281_L02_B5GHUMqzinRABFA-503_1.fq.gz has 84899558 lines; ./GW22-36/V350095281_L02_B5GHUMqzinRABFA-503_2.fq.gz has 0 lines
-./GW22-36/V350095281_L02_B5GHUMqzinRABFA-507_1.fq.gz has 0 lines; ./GW22-36/V350095281_L02_B5GHUMqzinRABFA-507_2.fq.gz has 203424196 lines
-./GW22-36/V350095281_L02_B5GHUMqzinRABFA-508_1.fq.gz has 0 lines; ./GW22-36/V350095281_L02_B5GHUMqzinRABFA-508_2.fq.gz has 175942060 lines
-./GW124-9/V350095281_L03_B5GHUMqzinRABGA-535_1.fq.gz has 89095293 lines; ./GW124-9/V350095281_L03_B5GHUMqzinRABGA-535_2.fq.gz has 0 lines
-./GW132-2B/V350095490_L01_B5GHUMqzinRABHA-542_1.fq.gz has 137866796 lines; ./GW132-2B/V350095490_L01_B5GHUMqzinRABHA-542_2.fq.gz has 0 lines
 
-
-ls ./*//GW21-6/V350095281_L01_B5GHUMqzinRABEA-582_*.fq.gz | parallel 'echo {} has $(zcat {} | wc -l) lines'
-
-ls ./*/GW3-7/V350082588_L03_B5GHUMqzinRABCA-570_*.fq.gz | parallel 'echo {} has $(zcat {} | wc -l) lines'
 
 
 get_counts () {
@@ -54,3 +43,9 @@ parallel -j8 get_counts {} ::: $(ls ./*/*/*_1.fq.gz| sort -V)
 parallel dowork ::: "${list[@]}" ::: "${other[@]}"
 
 ls ./*/*/*_1.fq.gz| sort -V
+
+
+
+for file in $(ls *.fq.gz| sort -V); do
+md5sum $file
+done
