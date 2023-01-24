69b5bdb361f353e84dbeb73cdea8d07e07036c89
Neupane <achalneupane@gmail.com>
2023-01-24 16:45:14 -0600

    .

--- rcodes/attributable_fraction_R_codes/V11_with_overlapping_PRS_variants_updated_lifestyle_from_Kiri-v2/SJLIFE_without_diet/4.f_subsequent_neoplasm_Meningioma_v11.r
+++ rcodes/attributable_fraction_R_codes/V11_with_overlapping_PRS_variants_updated_lifestyle_from_Kiri-v2/SJLIFE_without_diet/4.f_subsequent_neoplasm_Meningioma_v11.r
@@ -203,8 +203,8 @@ dat_all$pred_all = predict(fit_all, newdat = dat_all, type = "response")
 ## Move relevant treatment exposures for everyone to no exposure
 dat_tx = dat_all
 
-dat_tx$maxsegrtdose.category [!grepl("Unknown", dat_tx$maxsegrtdose.category)] =
-dat_tx$epitxn_dose_5.category [!grepl("Unknown", dat_tx$epitxn_dose_5.category)] = "None"
+dat_tx$maxsegrtdose.category =
+dat_tx$epitxn_dose_5.category = "None"
 
 dat_all$pred_no_tx = predict(fit_all, newdata = dat_tx, type = "response")
 
@@ -213,7 +213,7 @@ N_all = sum(dat_all$pred_all, na.rm = TRUE)
 N_no_tx = sum(dat_all$pred_no_tx, na.rm = TRUE)
 af_by_tx = (N_all - N_no_tx) / N_all
 round(af_by_tx,3)
-# 0.804
+# 0.818
 ##################
 ## P/LP and PRS ##
 ##################
@@ -232,26 +232,26 @@ round(af_by_plp.prs,3)
 ###############
 dat_lifestyle = dat_all
 
-dat_lifestyle$smoker_former_or_never_yn [!grepl("Unknown", dat_lifestyle$smoker_former_or_never_yn)] =
-dat_lifestyle$PhysicalActivity_yn [!grepl("Unknown", dat_lifestyle$PhysicalActivity_yn)] =
-dat_lifestyle$NOT_RiskyHeavyDrink_yn [!grepl("Unknown", dat_lifestyle$NOT_RiskyHeavyDrink_yn)] =
-# dat_lifestyle$HEALTHY_Diet_yn [!grepl("Unknown", dat_lifestyle$HEALTHY_Diet_yn)] =
-dat_lifestyle$Not_obese_yn [!grepl("Unknown", dat_lifestyle$Not_obese_yn)] = "1"
+dat_lifestyle$smoker_former_or_never_yn =
+dat_lifestyle$PhysicalActivity_yn =
+dat_lifestyle$NOT_RiskyHeavyDrink_yn =
+# dat_lifestyle$HEALTHY_Diet_yn =
+dat_lifestyle$Not_obese_yn = "1"
 
 
 dat_all$pred_no_favorable_lifestyle.category = predict(fit_all, newdata = dat_lifestyle, type = "response")
 N_no_favorable_lifestyle.category = sum(dat_all$pred_no_favorable_lifestyle.category, na.rm = TRUE)
 af_by_N_no_favorable_lifestyle.category = (N_all - N_no_favorable_lifestyle.category) / N_all
 round(af_by_N_no_favorable_lifestyle.category,3)
-# -0.014
+# 0.845
 #################################################
 ## Treatment, Genetics and Lifestyle, combined ##
 #################################################
 dat_tx.plp.prs.lifestyle = dat_all
 
 ## Nullify Treatment
-dat_tx.plp.prs.lifestyle$maxsegrtdose.category [!grepl("Unknown", dat_tx.plp.prs.lifestyle$maxsegrtdose.category)] =
-  dat_tx.plp.prs.lifestyle$epitxn_dose_5.category [!grepl("Unknown", dat_tx.plp.prs.lifestyle$epitxn_dose_5.category)] = "None"
+dat_tx.plp.prs.lifestyle$maxsegrtdose.category =
+  dat_tx.plp.prs.lifestyle$epitxn_dose_5.category = "None"
 
 
 ## Nullify Genetics
@@ -259,11 +259,11 @@ dat_tx.plp.prs.lifestyle$maxsegrtdose.category [!grepl("Unknown", dat_tx.plp.prs
 dat_tx.plp.prs.lifestyle$Meningioma_PRS.tertile.category = "1st"
 
 ## Nullify Lifestyle
-dat_tx.plp.prs.lifestyle$smoker_former_or_never_yn [!grepl("Unknown", dat_tx.plp.prs.lifestyle$smoker_former_or_never_yn)] =
-dat_tx.plp.prs.lifestyle$PhysicalActivity_yn [!grepl("Unknown", dat_tx.plp.prs.lifestyle$PhysicalActivity_yn)] =
-dat_tx.plp.prs.lifestyle$NOT_RiskyHeavyDrink_yn [!grepl("Unknown", dat_tx.plp.prs.lifestyle$NOT_RiskyHeavyDrink_yn)] =
-# dat_tx.plp.prs.lifestyle$HEALTHY_Diet_yn [!grepl("Unknown", dat_tx.plp.prs.lifestyle$HEALTHY_Diet_yn)] =
-dat_tx.plp.prs.lifestyle$Not_obese_yn [!grepl("Unknown", dat_tx.plp.prs.lifestyle$Not_obese_yn)] = "1"
+dat_tx.plp.prs.lifestyle$smoker_former_or_never_yn =
+dat_tx.plp.prs.lifestyle$PhysicalActivity_yn =
+dat_tx.plp.prs.lifestyle$NOT_RiskyHeavyDrink_yn =
+# dat_tx.plp.prs.lifestyle$HEALTHY_Diet_yn =
+dat_tx.plp.prs.lifestyle$Not_obese_yn = "1"
 
 
 dat_all$pred_no_favorable_lifestyle.category = predict(fit_all, newdata = dat_tx.plp.prs.lifestyle, type = "response")
@@ -271,4 +271,4 @@ dat_all$pred_no_favorable_lifestyle.category = predict(fit_all, newdata = dat_tx
 N_no_favorable_tx.plp.prs.lifestyle.category = sum(dat_all$pred_no_favorable_lifestyle.category, na.rm = TRUE)
 af_by_N_no_favorable_tx.plp.prs.lifestyle.category = (N_all - N_no_favorable_tx.plp.prs.lifestyle.category) / N_all
 round(af_by_N_no_favorable_tx.plp.prs.lifestyle.category,3)
-# 0.788
+# 0.99
