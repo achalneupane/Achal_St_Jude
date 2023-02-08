db087255b575bfb52531388f872045046be60ea9
Neupane <achalneupane@gmail.com>
2023-02-08 11:18:16 -0600

    .

--- rcodes/attributable_fraction_R_codes/replace_file_strings.sh
+++ rcodes/attributable_fraction_R_codes/replace_file_strings.sh
@@ -2,7 +2,5 @@ find ./ -type f| grep V11_with_overlapping| egrep -v "Pheno|Rhistory|Merge_all_a
 find ./ -type f| grep V11_with_overlapping| egrep -v "Pheno|Rhistory|Merge_all_admixture" | xargs -I {} sed -i 's/\bNOT_RiskyHeavyDrink_yn\b/RiskyHeavyDrink_yn/g' {}
 find ./ -type f| grep V11_with_overlapping| egrep -v "Pheno|Rhistory|Merge_all_admixture" | xargs -I {} sed -i 's/\bNot_obese_yn\b/Obese_yn/g' {}
 
-find ./ -type f| grep V11_with_overlapping| egrep -v "Pheno|Rhistory|Merge_all_admixture" | xargs -I {} sed -i 's/\bdat_tx.plp.prs.lifestyle$Current_smoker_yn =\b/dat_tx.plp.prs.lifestyle$Current_smoker_yn =/g' {}
 
-
-
+find ./ -type f| grep V11_with_overlapping| egrep -v "Pheno|Rhistory|Merge_all_admixture" | xargs -I {} sed -i sed 's/\bMavaddat_2019_ER_POS_Breast_PRS.tertile.category +//g' {}
