176a8326b33b7e4f6a0021197b67898afd42be74
Neupane <achalneupane@gmail.com>
2023-07-06 14:42:31 -0500

    .

--- rcodes/TITN_BAG3/combined_cohorts/TTN_BAG3_analysis_v2_07_06_2023.sh
+++ rcodes/TITN_BAG3/combined_cohorts/TTN_BAG3_analysis_v2_07_06_2023.sh
@@ -2,20 +2,51 @@
 ## Common variant analysis ##
 #############################
 ## On 02/20/2023, Combined all three datasets (sjlife, CCSS_org and CCSS_exp) and run the assocation analysis on the merged data; see Concatenated analysis on 02/20/2023 for phenotype file in 3.demographics_v2.R
+cd /research_jude/rgs01_jude/groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3
 plink --bfile sjlife --keep sjlife_ccss_org_ccss_exp_samples.txt --keep-allele-order --make-bed --out sjlife_to_concat
 plink --bfile ccss_org --keep sjlife_ccss_org_ccss_exp_samples.txt --keep-allele-order --make-bed --out ccss_org_to_concat
 plink --bfile ccss_exp --keep sjlife_ccss_org_ccss_exp_samples.txt --keep-allele-order --make-bed --out ccss_exp_to_concat
 
+
 ## Merge the above three
 plink --bfile sjlife_to_concat --merge-list merge_list.txt --keep-allele-order --out sjlife_ccss_org_ccss_exp_samples
 
+
+## extract problem variants
+grep "Warning" sjlife_ccss_org_ccss_exp_samples.log | awk -F "'" '{print $2}'|awk -F "[: ]" '{print $1":"$2}'|sort -V| uniq > warnings.txt
+
+
+## exclude problematic variants from merged file
+plink --bfile sjlife_ccss_org_ccss_exp_samples --exclude extract_vars.txt --make-bed --out good_dataset
+
+# ## update names in good ones
+plink --bfile good_dataset --update-name update_names_good_snps.txt --make-bed --out good_dataset_updated
+
+
+## extract problematic variants
+plink --bfile sjlife_ccss_org_ccss_exp_samples --extract extract_vars.txt --make-bed --out extracted_dataset
+
+
+
+plink --bfile extracted_dataset --update-alleles update_alleles.txt --make-bed --out extracted_dataset_updated_alleles
+
+
+## update names
+plink --bfile extracted_dataset_updated_alleles --update-name update_names.txt --make-bed --out extracted_dataset_updated_names
+
+
+
+## merge good datasets with updated dataset
+plink --bfile good_dataset_updated --bmerge  extracted_dataset_updated_names --make-bed --out sjlife_ccss_org_ccss_exp_samples_v2
+
+
 ## Calculate frequency
-plink --bfile sjlife_ccss_org_ccss_exp_samples --freq --keep-allele-order --out sjlife_ccss_org_ccss_exp_samples_freq_out
+plink --bfile sjlife_ccss_org_ccss_exp_samples_v2 --freq --keep-allele-order --out sjlife_ccss_org_ccss_exp_samples_freq_out_v2
 
 ## Run association analysis
  plink \
  --allow-no-sex \
- --bfile sjlife_ccss_org_ccss_exp_samples \
+ --bfile sjlife_ccss_org_ccss_exp_samples_v2 \
  --maf 0.01 \
  --hwe 1e-06 \
  --logistic hide-covar \
@@ -24,8 +55,10 @@ plink --bfile sjlife_ccss_org_ccss_exp_samples --freq --keep-allele-order --out 
  --pheno-name CMP \
  --covar pheno/sjlife_ccss_org_ccss_exp_ttn_bag3.pheno \
  --covar-name agedx,agelstcontact,gender,anthra_jco_dose_any,hrtavg,PC1,PC2,PC3,PC4,PC5,PC6,PC7,PC8,PC9,PC10 \
- --out sjlife_ccss_org_ccss_exp_samples_results
+ --out sjlife_ccss_org_ccss_exp_samples_results_v2
 
+
+Final analysis results are saved as sjlife_ccss_org_ccss_exp__ttn_bag3.assoc.final_v2_07_06_2023.xlxs
 
 
 
--- rcodes/TITN_BAG3/combined_cohorts/combine_variants_from_sjlife_ccss_exp_and_ccss_org.R
+++ rcodes/TITN_BAG3/combined_cohorts/combine_variants_from_sjlife_ccss_exp_and_ccss_org.R
@@ -1,3 +1,4 @@
+setwd("Z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3")
 df <- read.table("z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/sjlife_ccss_org_ccss_exp_samples.bim")
 warnings <- read.table("z:/ResearchHome/Groups/sapkogrp/projects/Cardiotoxicity/common/ttn_bag3/warnings.txt")
 dim(df)
@@ -39,7 +40,7 @@ dat$a2 <- dat$V6
 dat$a1.flipped <- sapply(dat$a1, flip_alleles)
 dat$a2.flipped <- sapply(dat$a2, flip_alleles)
 
-dat <- dat[1:1000,]
+# dat <- dat[1:1000,]
 
 
 dat$out.indicator <- ""  # Initialize the indicator column
@@ -61,26 +62,56 @@ for (i in 1:nrow(dat)) {
     }
     
     # Check if a1 and a2 already match
-    if (new.dat$a1[1] == new.dat$a1[j] && new.dat$a2[1] == new.dat$a2[j]) {
-      new.dat$out.indicator[j] <- "match"  # Update the indicator column for the first row
-      new.dat$out.new.a1[j] <- new.dat$a1[j]  # Update the new.a1 column with original a1 allele for the first row
-      new.dat$out.new.a2[j] <- new.dat$a2[j]  # Update the new.a2 column with original a2 allele for the first row
-    }
-    
-    if (new.dat$out.indicator[j] == "match") {
-      next  # Skip the row if it's already a match
-    }
-    
-    if (new.dat$a1[1] == new.dat$a2[j] && new.dat$a2[1] == new.dat$a1[j]) {
+    if (new.dat$a1[1] == new.dat$a1[j] && new.dat$a2[1] == new.dat$a2[j]) { ## direct match
+      new.dat$out.indicator[1] <- "match_row1"  # Update the indicator column for the first row
+      new.dat$out.new.a1[1] <- new.dat$a1[1]  # Update the new.a1 column with original a1 allele for the first row
+      new.dat$out.new.a2[1] <- new.dat$a2[1]  # Update the new.a2 column with original a2 allele for the first row
+      new.dat$out.indicator[j] <- "match"  # Update the indicator column for the j-th row
+      new.dat$out.new.a1[j] <- new.dat$a1[j]  # Update the new.a1 column with original a1 allele for the j-th row
+      new.dat$out.new.a2[j] <- new.dat$a2[j]  # Update the new.a2 column with original a2 allele for the j-th row
+    } else if (new.dat$a1[1] == new.dat$a2[j] && new.dat$a2[1] == new.dat$a1[j]) { ## swapped
+      new.dat$out.indicator[1] <- "swapped_row1"  # Update the indicator column
+      new.dat$out.new.a1[1] <- new.dat$a1[1]  # Update the new.a1 column with original a1 allele
+      new.dat$out.new.a2[1] <- new.dat$a2[1]  # Update the new.a2 column with original a2 allele
       new.dat$out.indicator[j] <- "swapped"  # Update the indicator column
-      new.dat$out.new.a1[j] <- new.dat$a1[j]  # Update the new.a1 column with original a1 allele
-      new.dat$out.new.a2[j] <- new.dat$a2[j]  # Update the new.a2 column with original a2 allele
-    } else if (new.dat$out.indicator[j] != "swapped" &&
-               new.dat$a1[1] == flip_alleles(new.dat$a2[j]) && new.dat$a2[1] == flip_alleles(new.dat$a1[j])) {
+      new.dat$out.new.a2[j] <- new.dat$a1[j]  # Update the new.a1 column with original a1 allele
+      new.dat$out.new.a1[j] <- new.dat$a2[j]  # Update the new.a2 column with original a2 allele
+    } else if (new.dat$a1[1] == flip_alleles(new.dat$a2[j]) && new.dat$a2[1] == flip_alleles(new.dat$a1[j])) { ## both swapped and flipped
+      new.dat$out.indicator[1] <- "swapped_flipped_row1"  # Update the indicator column
+      new.dat$out.new.a2[1] <- new.dat$a2[1]  # Update the new.a1 column with flipped allele
+      new.dat$out.new.a1[1] <- new.dat$a1[1]  # Update the new.a2 column with flipped allele
       new.dat$out.indicator[j] <- "swapped_flipped"  # Update the indicator column
       new.dat$out.new.a1[j] <- flip_alleles(new.dat$a2[j])  # Update the new.a1 column with flipped allele
       new.dat$out.new.a2[j] <- flip_alleles(new.dat$a1[j])  # Update the new.a2 column with flipped allele
-    }
+    } else if (new.dat$a1[1] == flip_alleles(new.dat$a1[j]) && new.dat$a2[1] == new.dat$a2[j]) { # a1 flipped
+    new.dat$out.indicator[1] <- "a1_flipped_row1"  # Update the indicator column
+    new.dat$out.new.a2[1] <- new.dat$a2[1]  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a1[1] <- new.dat$a1[1]  # Update the new.a2 column with flipped allele
+    new.dat$out.indicator[j] <- "a1_flipped"  # Update the indicator column
+    new.dat$out.new.a1[j] <- flip_alleles(new.dat$a1[j])  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a2[j] <- new.dat$a2[j]  # Update the new.a2 column with flipped allele
+    } else if (new.dat$a1[1] == new.dat$a1[j] && new.dat$a2[1] == flip_alleles(new.dat$a2[j])) { # a2 flipped
+    new.dat$out.indicator[1] <- "a2_flipped"  # Update the indicator column
+    new.dat$out.new.a2[1] <- new.dat$a2[1]  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a1[1] <- new.dat$a1[1]  # Update the new.a2 column with flipped allele
+    new.dat$out.indicator[j] <- "a2_flipped"  # Update the indicator column
+    new.dat$out.new.a1[j] <- new.dat$a1[j]  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a2[j] <- flip_alleles(new.dat$a2[j])  # Update the new.a2 column with flipped allele
+    } else if (new.dat$a1[1] == new.dat$a2[j] && new.dat$a2[1] == flip_alleles(new.dat$a1[j])) { # a1 swapped and flipped
+    new.dat$out.indicator[1] <- "a1_flipped_swapped_row1"  # Update the indicator column
+    new.dat$out.new.a2[1] <- new.dat$a2[1]  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a1[1] <- new.dat$a1[1]  # Update the new.a2 column with flipped allele
+    new.dat$out.indicator[j] <- "a1_flipped_swapped"  # Update the indicator column
+    new.dat$out.new.a1[j] <- new.dat$a2[j]  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a2[j] <- flip_alleles(new.dat$a1[j])  # Update the new.a2 column with flipped allele
+    } else if (new.dat$a1[1] == flip_alleles(new.dat$a2[j]) && new.dat$a2[1] == new.dat$a1[j]) { # a2 swapped and flipped
+    new.dat$out.indicator[1] <- "a2_flipped_swapped_row1"  # Update the indicator column
+    new.dat$out.new.a2[1] <- new.dat$a2[1]  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a1[1] <- new.dat$a1[1]  # Update the new.a2 column with flipped allele
+    new.dat$out.indicator[j] <- "a2_flipped_swapped"  # Update the indicator column
+    new.dat$out.new.a2[j] <- new.dat$a1[j]  # Update the new.a1 column with flipped allele
+    new.dat$out.new.a1[j] <- flip_alleles(new.dat$a2[j])  # Update the new.a2 column with flipped allele
+  }
   }
   
   
@@ -90,3 +121,30 @@ for (i in 1:nrow(dat)) {
   dat[grepl(KEYpos, dat$KEY), "out.new.a1"] <- new.dat$out.new.a1
   dat[grepl(KEYpos, dat$KEY), "out.new.a2"] <- new.dat$out.new.a2
 }
+
+dat.final <- dat[!dat$out.indicator=="",]
+
+first.row <- dat.final[grepl("_row", dat.final$out.indicator),]
+dat.final <- dat.final[!grepl("_row", dat.final$out.indicator),]
+
+
+## extract variants
+write.table(dat.final$V2, "extract_vars.txt", col.names = F, quote = F, row.names = F)
+## update alleles
+update_allele <- cbind.data.frame(dat.final$V2, dat.final$a2, dat.final$a1, dat.final$out.new.a2, dat.final$out.new.a1)
+write.table(update_allele, "update_alleles.txt", col.names = F, quote = F, row.names = F)
+
+## update names
+update_names <- cbind.data.frame(OLD=dat.final$V2, NEW=paste0(dat.final$KEY, ":", dat.final$out.new.a2, ":", dat.final$out.new.a1))
+
+## check if names are same
+# update_names$MATCH <- update_names[1] == update_names[2]
+
+# update_names$NEW[grepl("chr2:178316698:",update_names$OLD)] <- "chr2:178316698:C:T"
+# update_names$NEW[grepl("chr2:178313779:",update_names$OLD)] <- "chr2:178313779:A:G"
+
+write.table(update_names, "update_names.txt", col.names = F, quote = F, row.names = F)
+
+# ## update names in the good ones
+update_names_good_snps <- cbind.data.frame(OLD = first.row$V2, NEW=paste0(first.row$KEY, ":", first.row$out.new.a2, ":", first.row$out.new.a1))
+write.table(update_names_good_snps, "update_names_good_snps.txt", col.names = F, quote = F, row.names = F)
