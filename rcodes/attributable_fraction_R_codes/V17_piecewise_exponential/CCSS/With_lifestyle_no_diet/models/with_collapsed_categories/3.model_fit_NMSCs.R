fd71d63a7087cce3737d404470728660819a7c47
Neupane <achalneupane@gmail.com>
2023-08-16 13:54:19 -0500

    .

--- rcodes/attributable_fraction_R_codes/V17_piecewise_exponential/CCSS/With_lifestyle_no_diet/models/with_collapsed_categories/3.model_fit_NMSCs.R
+++ rcodes/attributable_fraction_R_codes/V17_piecewise_exponential/CCSS/With_lifestyle_no_diet/models/with_collapsed_categories/3.model_fit_NMSCs.R
@@ -1,5 +1,5 @@
 # load ANY SN data
-load("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/PHENOTYPE/ccss.NMSCs.V17_without_diet.Rdata")
+load("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/PHENOTYPE/ccss.NMSCs.V17b_without_diet.Rdata")
 
 # Yutaka's email on 03/16/2023:  It seems maxsegrtdose 0-18 Gy is a very small group and perhaps needs to be combined with 18-30 Gy
 cc
