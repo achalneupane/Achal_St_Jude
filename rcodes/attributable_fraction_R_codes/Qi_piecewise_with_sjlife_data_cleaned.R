3c28d72a63c82d75a60946169b3e85a91abf408a
Neupane <achalneupane@gmail.com>
2023-09-21 09:17:14 -0500

    .

--- rcodes/attributable_fraction_R_codes/Qi_piecewise_with_sjlife_data_cleaned.R
+++ rcodes/attributable_fraction_R_codes/Qi_piecewise_with_sjlife_data_cleaned.R
@@ -11,10 +11,32 @@ library(lubridate)
 # benchmarkme::get_ram()
 library(survival)
 
+## function to add cubic spline
+cubic_spline <- function(tp, knots)
+{
+  n=length(knots)
+  f=rep(0,times=length(tp)*(n-1))
+  dim(f)=c(length(tp),(n-1))
+  #f1(x)=x#
+  f[,1]=tp
+  for(j in 1:(n-2))
+  {
+    for (i in 1:length(tp))
+    {
+      f[i,(j+1)]=max(0,(tp[i]-knots[j])^3, na.rm=TRUE)-max(0,(tp[i]-knots[n-1])^3, na.rm=TRUE)*(knots[n]-knots[j])/(knots[n]-knots[n-1])+
+        max(0,(tp[i]-knots[n])^3, na.rm=TRUE)*(knots[n-1]-knots[j])/(knots[n]-knots[n-1])
+    }
+  }
+  return(f)
+}
+
+
 data <- read_sas("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/PHENOTYPE/Data_from_Qi_Liu/toyadav.sas7bdat")
+ethnicity.admixture <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/admixture/merged.ancestry.file.txt", header = T)
+data <- cbind.data.frame(data, ethnicity.admixture[match(data$sjlid, ethnicity.admixture$INDIVIDUAL), c("EUR", "EAS", "AFR")])
+
 
 # data$agelstcontact.origina <- data$agelstcontact
-
 data$gradedt <- data$evaldt ## SN grade date
 
 ## Age at SN diagnosis
@@ -25,14 +47,12 @@ data$agelstcontact[!is.na(data$evaldt)] <- data$AGE.ANY_SN[!is.na(data$AGE.ANY_S
 
 
 ## Add cubic spline of age attained 
-source("Z:/ResearchHome/ClusterHome/aneupane/St_Jude/Achal_St_Jude/rcodes/attributable_fraction_R_codes/cubic_spline.r")
 breaks = seq(5, 95, 22.5)
 cp = quantile(data$agelstcontact, breaks/100, na.rm = T)
 cs = cubic_spline(data$agelstcontact, knots = cp)
 
 colnames(cs) <- c("AGE_AT_LAST_CONTACT.cs1", "AGE_AT_LAST_CONTACT.cs2", "AGE_AT_LAST_CONTACT.cs3", "AGE_AT_LAST_CONTACT.cs4")
 data <- cbind.data.frame(data, cs)
-# cs.data1 <- data[c("sjlid", "AGE_AT_LAST_CONTACT.cs1", "AGE_AT_LAST_CONTACT.cs2", "AGE_AT_LAST_CONTACT.cs3", "AGE_AT_LAST_CONTACT.cs4")]
 
 
 ## define event (Any SN)
@@ -56,7 +76,6 @@ alldata <- merge(data,event.number,by.x="sjlid",by.y="sjlid")
 alldata$start <- NULL
 alldata$end <- NULL
 ### For those without event, start is agedx and end is Fu date === for SN, analysis starts from 5 years post DX (SNs within 5 years have been removed from the analysis)
-### Achal: Since the analysis start from 5 years post DX, this line, alldata$start[alldata$event==0] <- alldata$agedx[alldata$event==0], has been revised, as below:
 alldata$start[alldata$event==0] <- alldata$agedx[alldata$event==0] + 5
 alldata$end[alldata$event==0] <- alldata$agelstcontact[alldata$event==0] 
 
@@ -70,11 +89,9 @@ gg1 <-cbind.data.frame(alldata$sjlid, alldata$event, alldata$first, alldata$prev
 alldata[1:10,]
 ### if first>1 (i.e, 2 or more events, previous event time remained, others are missing)
 alldata$previous[alldata$first==1] <- NA
-gg2 <-cbind.data.frame(alldata$sjlid, alldata$event, alldata$first, alldata$previous, alldata$gradedt, alldata$start, alldata$end)
 
 alldata$start[alldata$first>1] <- as.numeric(difftime(alldata$previous[alldata$first>1],alldata$dob[alldata$first>1], units = 'days')/365.25)
 alldata$end[alldata$first>1] <- as.numeric(difftime(alldata$gradedt[alldata$first>1],alldata$dob[alldata$first>1], units = 'days')/365.25)
-cc1 <- alldata[c(1,50:ncol(alldata))]
 
 ### If one person has only 1 event, we need to have segment from agedx to event date (handled above), and then from event date to end of FU
 row_add <- alldata[alldata$event==1 & alldata$first==alldata$maxE,] ## this includes (1) people with only 1 event (2) The last row for people with multiple events
@@ -92,20 +109,13 @@ adata <- rbind(alldata,row_add)
 #### order by person and start date
 adata <- adata[order(adata$sjlid, adata$start, decreasing = FALSE),]
 table(adata$event)## Double check event numebr is correct
-cc2 <- adata[c(1,50:ncol(alldata))]
 
 ###any stop time <=start time
-# valid_indices <- !is.na(adata$end) & adata$end <= adata$start
-# adata$end[valid_indices] <- adata$end[valid_indices] + 1/365
-adata$end[adata$end<=adata$start] <- adata$end[adata$end<=adata$start] + 1/365
+# adata$end[adata$end<=adata$start] <- adata$end[adata$end<=adata$start] + 1/365
 any <- adata[adata$end<=adata$start,] # 
 diff=any$start-any$end 
 dim(adata)
 final <- adata[adata$end>adata$start,]
-
-# final$sjlid[duplicated(final$sjlid)]
-# cc.final <- final[, c(1,(ncol(final)-11):ncol(final))]
-# cc.data <- data[, c(1,(ncol(data)-11):ncol(data))]
 
 minimum  <-  min(final$start, na.rm = TRUE) - 1
 if (minimum < 0) minimum <- 0
@@ -118,40 +128,29 @@ length(unique(SNs_py$sjlid[SNs_py$event==1 & SNs_py$evt1==1 ]))
 
 SNs_py$PY <- SNs_py$end-SNs_py$start
 
+
 SNs_py2=SNs_py
-# SNs_py2=SNs_py[SNs_py$evt1==1,]
+# SNs_py2=SNs_py[SNs_py$evt1==1,] 
 
 
 SNs_py2$agedxcat <- factor(SNs_py2$agedxcat)
 SNs_py2$sex <- factor(SNs_py2$sex)
 SNs_py2$sex <- relevel(SNs_py2$sex, ref = "Male")
-
 SNs_py2$braincat <- factor(SNs_py2$braincat)
 SNs_py2$abdcat <- factor(SNs_py2$abdcat)
 SNs_py2$chestcat <- factor(SNs_py2$chestcat)
 SNs_py2$pelviscat <- factor(SNs_py2$pelviscat)
 SNs_py2$neckcat <- factor(SNs_py2$neckcat)
-
 SNs_py2$cat_anthra_jco_dose <- factor(SNs_py2$cat_anthra_jco_dose)
 SNs_py2$cat_epitxn_dose <- factor(SNs_py2$cat_epitxn_dose)
 SNs_py2$cat_aa_class_dose <- factor(SNs_py2$cat_aa_class_dose)
 
-
-ethnicity.admixture <- read.table("Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/attr_fraction/admixture/merged.ancestry.file.txt", header = T)
-SNs_py2 <- cbind.data.frame(SNs_py2, ethnicity.admixture[match(SNs_py2$sjlid, ethnicity.admixture$INDIVIDUAL), c("EUR", "EAS", "AFR")])
 
 
 ###############
 ## Model fit ##
 ###############
 ## 1. Any SN
-
-# SNs_py2$any_chemo_missing <- apply(SNs_py2[c("cat_epitxn_dose")], 1, function(x) any("Unknown" %in% x))
-# SNs_py2$any_chemo_missing  <- factor(ifelse(SNs_py2$any_chemo_missing == FALSE, "No", "Yes"))
-# 
-# SNs_py2$any_rt_missing <- apply(SNs_py2[c("braincat", "abdcat", "chestcat")], 1, function(x) any("Unknown" %in% x))
-# SNs_py2$any_rt_missing  <- factor(ifelse(SNs_py2$any_rt_missing == FALSE, "No", "Yes"))
-
 fit_all <- glm(formula = event ~ AGE_AT_LAST_CONTACT.cs1 + AGE_AT_LAST_CONTACT.cs2 + AGE_AT_LAST_CONTACT.cs3 + AGE_AT_LAST_CONTACT.cs4 +
                 agedxcat + sex +
                 braincat + abdcat + chestcat + cat_epitxn_dose +
@@ -185,201 +184,4 @@ results.anySN <- data.frame(
 # Print the results
 print(results.anySN)
 
-## 2. Meningioma
-fit_all <- glm(formula = event ~ AGE_AT_LAST_CONTACT.cs1 + AGE_AT_LAST_CONTACT.cs2 + AGE_AT_LAST_CONTACT.cs3 + AGE_AT_LAST_CONTACT.cs4 +
-                 agedxcat + sex +
-                 braincat + cat_epitxn_dose +
-               family = "poisson", offset = log(SNs_py2$PY), data = SNs_py2)
 
-summary_fit_all <- summary(fit_all)
-
-
-# Extract coefficients, standard errors, and p-values from the model summary
-coefficients <- coef(summary_fit_all)
-std_errors <- coefficients[, "Std. Error"]
-p_values <- coefficients[, "Pr(>|z|)"]
-
-# Calculate relative risks (exponentiated coefficients)
-relative_risks <- exp(coefficients[, "Estimate"])
-
-# Calculate 95% confidence intervals for relative risks
-conf_int_low <- exp(coefficients[, "Estimate"] - 1.96 * std_errors)
-conf_int_high <- exp(coefficients[, "Estimate"] + 1.96 * std_errors)
-
-# Create a data frame to display the results
-results.meningioma <- data.frame(
-  Coefficient = rownames(coefficients),
-  Relative_Risk = relative_risks,
-  CI_Lower = conf_int_low,
-  CI_Upper = conf_int_high,
-  P_Value = p_values
-)
-
-# Print the results
-print(results.meningioma)
-
-
-
-
-
-
-# ################################################################################
-# ## Breast cancer
-# breast <- QIsubneo[QIsubneo$sndx == 0 | QIsubneo$sndx == 5,]
-# table(breast$sndx)
-# data <- QIsubneo
-# data$event <- ifelse(data$sndx == 0, 0, 1)
-# data$gradedt <- data$evaldt
-# 
-# 
-# 
-# ## Qi's code to piece-wise
-# data$first <- ave(data$agelstcontact, data$sjlid, FUN = seq_along)
-# M <- max(data$first, na.rm = T)  ### maximum number of events
-# data[data$first==M,]$sjlid  #the id for the person with the maximum number of rows.
-# data[data$sjlid==data[data$first==M,]$sjlid,]
-# 
-# ### Take the last row so we know the maximum number of events per person
-# event.number <- do.call(rbind, lapply(split(data, data$sjlid), tail, 1))[,c("sjlid","first")]
-# colnames(event.number) <- c("sjlid","maxE")
-# 
-# alldata <- merge(data,event.number,by.x="sjlid",by.y="sjlid")
-# 
-# alldata$start <- NULL
-# alldata$end <- NULL
-# ### For those without event, start is agedx and end is Fu date === for SN, analysis starts from 5 years post DX (SNs within 5 years have been removed from the analysis)
-# alldata$start[alldata$event==0] <- alldata$agedx[alldata$event==0] + 5
-# ### Achal: Since the analysis start from 5 years post DX, the above line has been revised to: alldata$start[alldata$event==0] <- alldata$agedx[alldata$event==0]+5
-# alldata$end[alldata$event==0] <- alldata$agelstcontact[alldata$event==0] 
-# 
-# ### For the first event, start is agedx and end is first event time
-# alldata$start[alldata$event==1 & alldata$first==1] <- alldata$agedx[alldata$event==1 & alldata$first==1] + 5
-# ### Achal: also added +5 in the above line
-# alldata$end[alldata$event==1 & alldata$first==1] <- as.numeric(difftime(alldata$gradedt[alldata$event==1 & alldata$first==1],alldata$dob[alldata$event==1 & alldata$first==1], units = 'days')/365.25)
-# 
-# #### For events that are not the first, segments are from the previous event date to this event date
-# alldata$previous <- as.Date(c(NA,alldata$gradedt[1:length(alldata$gradedt)-1]),origin = "1970-01-01")
-# alldata[1:10,]
-# ### if first>1 (i.e, 2 or more events, previous event time remained, others are missing)
-# alldata$previous[alldata$first==1] <- NA
-# alldata$start[alldata$first>1] <- as.numeric(difftime(alldata$previous[alldata$first>1],alldata$dob[alldata$first>1], units = 'days')/365.25)
-# alldata$end[alldata$first>1] <- as.numeric(difftime(alldata$gradedt[alldata$first>1],alldata$dob[alldata$first>1], units = 'days')/365.25)
-# 
-# 
-# ### If one person has only 1 event, we need to have segment from agedx to event date (handled above), and then from event date to end of FU
-# ### If one person has multiple events, we need to add segments from the last event to end of Fu.
-# row_add <- alldata[alldata$event==1 & alldata$first==alldata$maxE,] ## this includes (1) people with only 1 event (2) The last row for people with multiple events
-# row_add$start <- as.numeric(difftime(row_add$gradedt, row_add$dob, units = 'days')/365.25)
-# row_add$end <- row_add$agelstcontact;
-# #### remember to make these rows event as 0, as we are adding the time after the last event date.
-# row_add$event <- 0
-# ### If we do first event analysis, we would not need these. I need to add an indicator here, in case I only need to get the first event analysis segments.
-# row_add$evt1 <- 0;
-# 
-# alldata$evt1 <- 0;
-# alldata$evt1[alldata$first==1] <- 1
-# adata <- rbind(alldata,row_add)
-# #### order by person and start date
-# adata <- adata[order(adata$sjlid, adata$start, decreasing = FALSE),]
-# table(adata$event)## Double check event numebr is correct
-# 
-# ###any stop time <=start time
-# adata$end[adata$end<=adata$start] <- adata$end[adata$end<=adata$start] + 1/365
-# any <- adata[adata$end<=adata$start,] # 
-# ### 2 people had the stroke date on the Fu date. In this case, either get rid of the 2 lines [i.e, no time is follow-up after the last event date], or add 1 day on end date of these 2 segments, assuming there were followed up 1 more day. Will not make much difference. 1 day out of 365 days is 0.0027.
-# diff=any$start-any$end ###Qi: These are people who had SN after the last contact date. Just wonder why this could happen. While it may not make the results differ, I wonder is there any reason to keep the events but change last contact date to be SN+1day? Depends on why there are SN after last contact date.
-# dim(adata)
-# final <- adata[adata$end>adata$start,]
-# 
-# minimum  <-  min(final$start, na.rm = TRUE) - 1
-# if (minimum < 0) minimum <- 0
-# maximum  <-  max(final$end, na.rm = TRUE) + 1
-# SNs_py <-  survSplit(final, cut=seq(minimum, maximum, 1), end="end",start="start",event="event") 
-# table(SNs_py$event)   ## Double check event numebr is correct
-# #### If you need the rows for first event analysis, take evt1=1
-# length(unique(SNs_py$sjlid[SNs_py$event==1]))
-# length(unique(SNs_py$sjlid[SNs_py$event==1 & SNs_py$evt1==1 ]))
-# 
-# SNs_py$PY <- SNs_py$end-SNs_py$start
-# 
-# SNs_py.original <- SNs_py
-# ###############
-# ## Model fit ##
-# ###############
-# ## Model for breast cancer
-# SNs_py <- SNs_py[c("sjlid", "event",
-#                    "AGE_AT_LAST_CONTACT.cs1", "AGE_AT_LAST_CONTACT.cs2", "AGE_AT_LAST_CONTACT.cs3", "AGE_AT_LAST_CONTACT.cs4",
-#                    "agedxcat", "sex", 
-#                    "chestcat", "cat_anthra_jco_dose", 
-#                    "PY","evt1")]
-# 
-# SNs_py <- SNs_py[complete.cases(SNs_py),]
-# 
-# 
-# SNs_py$agedxcat <- factor(SNs_py$agedxcat)
-# SNs_py$sex <- factor(SNs_py$sex)
-# SNs_py$cat_anthra_jco_dose <- factor(SNs_py$cat_anthra_jco_dose)
-# SNs_py$chestcat <- factor(SNs_py$chestcat)
-# 
-# ### Qi output the data to fit in SAS.
-# # write.csv(SNs_py,file="R:/Biostatistics/Biostatistics2/Qi/QiCommon/St Jude/Achal/data.csv")
-# 
-# library("geepack")
-# # fit_all <- geeglm(formula = event ~ AGE_AT_LAST_CONTACT.cs1 + AGE_AT_LAST_CONTACT.cs2 + AGE_AT_LAST_CONTACT.cs3 + AGE_AT_LAST_CONTACT.cs4 +
-# #                     agedxcat + sex + 
-# #                     chestcat + cat_anthra_jco_dose,
-# #                     family = "poisson", id = SNs_py$sjlid, offset = log(SNs_py$PY), corstr = "independence",  std.err = "san.se", data = SNs_py)
-# # #####Qi: I tried the above model in SAS. If using corstr=cs in SAS, it had convergence issue. If I used type=ind then it converged in SAS. PRS not significant though, with p-value 0.17 and 0.14 below from SAS.
-# # # Pleiotropy_PRSWEB_PR 2nd        0.2368   0.1729  -0.1021   0.5758    1.37   0.1709
-# # # Pleiotropy_PRSWEB_PR 3rd        0.2401   0.1646  -0.0826   0.5628    1.46   0.1448
-# # ######## " AGE_AT_LAST_CONTACT.cs1", I  guess these are age cubic splines. One thing you can try is to make the age as categorical. For example, then one person would have many rows with the same age category. For example, one had 10 rows in 20-29 years old. Then you can collapse the rows into 1 row because the 10 rows have all X's the same. event=sum(event) and py=sum(py) over the 10 rows. In this way, you make your rows much smaller and will help with the convergence issue.
-# 
-# 
-# ##### Qi: 	   
-# ##### if not GEE (do first event analysis), the model works and here is how to get PAF.	   
-# SNs_py2=SNs_py[SNs_py$evt1==1,]
-# fit_all <- glm(formula = event ~ AGE_AT_LAST_CONTACT.cs1 + AGE_AT_LAST_CONTACT.cs2 + AGE_AT_LAST_CONTACT.cs3 + AGE_AT_LAST_CONTACT.cs4 +
-#                                      agedxcat + sex +
-#                                      chestcat + cat_anthra_jco_dose,
-#                family = "poisson", offset = log(SNs_py2$PY), data = SNs_py2)
-# 
-# summary_fit_all <- summary(fit_all)
-# 
-# 
-# # Extract coefficients, standard errors, and p-values from the model summary
-# coefficients <- coef(summary_fit_all)
-# std_errors <- coefficients[, "Std. Error"]
-# p_values <- coefficients[, "Pr(>|z|)"]
-# 
-# # Calculate relative risks (exponentiated coefficients)
-# relative_risks <- exp(coefficients[, "Estimate"])
-# 
-# # Calculate 95% confidence intervals for relative risks
-# conf_int_low <- exp(coefficients[, "Estimate"] - 1.96 * std_errors)
-# conf_int_high <- exp(coefficients[, "Estimate"] + 1.96 * std_errors)
-# 
-# # Create a data frame to display the results
-# results <- data.frame(
-#   Coefficient = rownames(coefficients),
-#   Relative_Risk = relative_risks,
-#   CI_Lower = conf_int_low,
-#   CI_Upper = conf_int_high,
-#   P_Value = p_values
-# )
-# 
-# # Print the results
-# print(results)
-# 
-# 
-# 
-# #### to get PAF. 
-# #### First get the precited count from the original data and fit, the expected count should be the same as the # of events.	   
-# aa=predict(fit_all,type="response")   
-# sum(aa)	   
-# 
-# #### Assume you are interested in the PRS PAF. Make a fake new data, all people are in the reference of PRS.	   
-# pynew=SNs_py2
-# pynew$Pleiotropy_PRSWEB_PRS.tertile.category="1st"
-# table(pynew$Pleiotropy_PRSWEB_PRS.tertile.category)
-# bb=predict(fit_all,type="response",newdata=pynew)
-# PAF=(sum(aa)-sum(bb))/sum(aa)  ##about 15%
