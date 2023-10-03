8d60e554c3d63e53a8d25b0d6367d6f6e1722c1c
Neupane <achalneupane@gmail.com>
2023-10-03 11:44:24 -0500

    .

--- rcodes/attributable_fraction_R_codes/V18/18b/CCSS/Without_lifestyle/models/with_collapsed_categories/4.model_fit_BREASTcancer.R
+++ rcodes/attributable_fraction_R_codes/V18/18b/CCSS/Without_lifestyle/models/with_collapsed_categories/4.model_fit_BREASTcancer.R
@@ -24,6 +24,7 @@ PHENO.ANY_SN$anthra_jco_dose_5.category <- factor(PHENO.ANY_SN$anthra_jco_dose_5
 ## Attributable fraction of Any SNs ##
 ######################################
 dat_all = PHENO.ANY_SN
+dat_all = dat_all[dat_all$gender == "Female",]
 dat_all=dat_all[dat_all$evt1==1,]
 
 fit_all = glm(formula = event ~ 
@@ -307,6 +308,7 @@ Breast.res <- data.frame(
 # ## Check PRS and treatment interaction ##
 # #########################################
 # dat_all = PHENO.ANY_SN
+dat_all = dat_all[dat_all$gender == "Female",]
 # dat_all=dat_all[dat_all$evt1==1,]
 # 
 # fit_all = glm(formula = event ~ 
