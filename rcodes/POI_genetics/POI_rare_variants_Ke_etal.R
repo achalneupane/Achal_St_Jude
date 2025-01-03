17a88af41e3b3da8dcd973d99ea6cec919739913
Neupane <achalneupane@gmail.com>
2025-01-03 14:58:31 -0600

    .

--- rcodes/POI_genetics/POI_rare_variants_Ke_etal.R
+++ rcodes/POI_genetics/POI_rare_variants_Ke_etal.R
@@ -409,6 +409,7 @@ length(snps.with.carriers.snpeff.afr)
 ## 13
 # carriers.snpeff.afr$All_Genes <- ifelse(rowSums(carriers.snpeff.afr[ , -1], na.rm = TRUE) > 0, 1, 0)
 
+
 # Save each object as an RDS file in the specified path
 saveRDS(carriers.clinvar.eur, file = "Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/POI_genetics/Ke_et_al_POI_genetics_carriers_clinvar_eur.rds")
 saveRDS(carriers.clinvar.afr, file = "Z:/ResearchHome/Groups/sapkogrp/projects/Genomics/common/POI_genetics/Ke_et_al_POI_genetics_carriers_clinvar_afr.rds")
